bf1c67b55534fcee535ddfaff58ae2caae77c3dc
sdk-team <sdk-team@alibabacloud.com>
2020-09-08 02:34:47 +0000

    Support adding or removing vm app to mesh.

---
+++ servicemesh-20200111/core/Models/GetVmMetaRequest.cs
@@ -0,0 +1,30 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AlibabaCloud.SDK.Servicemesh20200111.Models
+{
+    public class GetVmMetaRequest : TeaModel {
+        [NameInMap("ServiceMeshId")]
+        [Validation(Required=true)]
+        public string ServiceMeshId { get; set; }
+
+        [NameInMap("TrustDomain")]
+        [Validation(Required=false)]
+        public string TrustDomain { get; set; }
+
+        [NameInMap("Namespace")]
+        [Validation(Required=false)]
+        public string Namespace { get; set; }
+
+        [NameInMap("ServiceAccount")]
+        [Validation(Required=false)]
+        public string ServiceAccount { get; set; }
+
+    }
+
+}
