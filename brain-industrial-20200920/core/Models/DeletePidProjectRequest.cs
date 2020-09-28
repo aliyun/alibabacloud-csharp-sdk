f399964f172a80721d15f655ff44db710334a78a
sdk-team <sdk-team@alibabacloud.com>
2020-09-28 07:29:23 +0000

    Supported APIs for PidOrganization.

---
+++ brain-industrial-20200920/core/Models/DeletePidProjectRequest.cs
@@ -0,0 +1,18 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AlibabaCloud.SDK.Brain_industrial20200920.Models
+{
+    public class DeletePidProjectRequest : TeaModel {
+        [NameInMap("PidProjectId")]
+        [Validation(Required=true)]
+        public string PidProjectId { get; set; }
+
+    }
+
+}
---
+++ brain-industrial-20200920/core/Models/GetPidLoopRequest.cs
@@ -0,0 +1,18 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AlibabaCloud.SDK.Brain_industrial20200920.Models
+{
+    public class GetPidLoopRequest : TeaModel {
+        [NameInMap("PidLoopId")]
+        [Validation(Required=true)]
+        public string PidLoopId { get; set; }
+
+    }
+
+}
