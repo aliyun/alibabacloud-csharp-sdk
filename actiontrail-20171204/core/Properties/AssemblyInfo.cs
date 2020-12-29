aa772682d970d22e69c91729eec50afd1d81769d
sdk-team <sdk-team@alibabacloud.com>
2020-12-29 02:40:22 +0000

    AMP Version Change.

--- actiontrail-20171204/core/Properties/AssemblyInfo.cs
+++ actiontrail-20171204/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("74ee9d20-48fe-11eb-a092-6d4f1090217f")]
+[assembly: Guid("2aed96a0-497f-11eb-babc-774b53064c41")]
 
 // Version information for an assembly consists of the following four values:
 //
