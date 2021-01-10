f9c16f38cfe7a548b4e1fee809a1cbdcea1797c3
sdk-team <sdk-team@alibabacloud.com>
2021-01-10 07:07:13 +0000

    Generated csharp 2018-05-01 for sms-intl.

--- sms-intl-20180501/ChangeLog.md
+++ sms-intl-20180501/ChangeLog.md
@@ -1,3 +1,6 @@
+2021-01-10 Version: 1.0.1
+- Generated csharp 2018-05-01 for sms-intl.
+
 2021-01-09 Version: 1.0.0
 - Generated csharp 2018-05-01 for sms-intl.
 
--- sms-intl-20180501/core/Properties/AssemblyInfo.cs
+++ sms-intl-20180501/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("94ea6030-5288-11eb-b59d-f30af7ee0c19")]
+[assembly: Guid("6f099db0-5312-11eb-9955-5786b1f19eaf")]
 
 // Version information for an assembly consists of the following four values:
 //
