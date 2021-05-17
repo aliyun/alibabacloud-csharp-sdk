e32a34b5ee1b4dfed4676ce5ce106f8883b94162
sdk-team <sdk-team@alibabacloud.com>
2021-05-17 04:08:41 +0000

    Generated csharp 2018-12-03 for Sas.

--- sas-20181203/core/Properties/AssemblyInfo.cs
+++ sas-20181203/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("eb908ef0-b4b2-11eb-880a-59ceafc4776c")]
+[assembly: Guid("81a10c80-b6c5-11eb-8163-a7291a9e2b50")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
