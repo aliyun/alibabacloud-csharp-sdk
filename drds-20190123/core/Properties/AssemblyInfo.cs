aa9a0c068714c97cd5d553a1eb9d86103bff6147
sdk-team <sdk-team@alibabacloud.com>
2021-05-23 10:20:29 +0000

    New API- GetDrdsDbRdsRelationInfo, which can get private RDS list under certain DB out of DRDS instance ID and DB name.

--- drds-20190123/core/Properties/AssemblyInfo.cs
+++ drds-20190123/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cc3cf560-b481-11eb-929e-ab0680139ae4")]
+[assembly: Guid("6f757920-bbb0-11eb-92ad-17ff9d62f486")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
