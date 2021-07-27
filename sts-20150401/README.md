9e9d0fd8cf4193ff5a1262e4b268c4b102c9f848
sdk-team <sdk-team@alibabacloud.com>
2021-07-27 02:52:43 +0000

    Generated csharp 2015-04-01 for Sts.

--- sts-20150401/README.md
+++ sts-20150401/README.md
@@ -6,10 +6,10 @@ English | [简体中文](README-CN.md)
 
 ## Installation
 
-### Composer
+### Nuget
 
 ```bash
-composer require AlibabaCloud.SDK.Sts20150401
+dotnet add package AlibabaCloud.SDK.Sts20150401
 ```
 
 ## Issues
--- sts-20150401/core/Properties/AssemblyInfo.cs
+++ sts-20150401/core/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("alibabacloud-sts")]
 [assembly: AssemblyDescription("Alibaba Cloud Sts (20150401) SDK Library for .NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Alibaba Cloud, Inc")]
+[assembly: AssemblyCompany("Alibaba Cloud")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("343a4d40-4a4f-11eb-be2f-793265280392")]
+[assembly: Guid("aebc2900-ee85-11eb-9746-3556dbef8d6e")]
 
 // Version information for an assembly consists of the following four values:
 //
