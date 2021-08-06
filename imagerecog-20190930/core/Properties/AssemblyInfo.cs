a47e92f2ae51ad862710e589009dfcaab4d56ac7
sdk-team <sdk-team@alibabacloud.com>
2021-08-06 05:32:36 +0000

    Update TaggingAdImage.

--- imagerecog-20190930/core/Properties/AssemblyInfo.cs
+++ imagerecog-20190930/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5aa0f1e0-f509-11eb-8a66-43a287f5560a")]
+[assembly: Guid("ac257850-f677-11eb-a40b-cbdcbb1e4a1b")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
