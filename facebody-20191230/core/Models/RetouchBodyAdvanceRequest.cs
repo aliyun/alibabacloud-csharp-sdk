e95703815fddc96150a0c50348ef98652eff37f4
sdk-team <sdk-team@alibabacloud.com>
2021-08-24 06:58:11 +0000

    Release RetouchBody.

---
+++ facebody-20191230/core/Models/RetouchBodyAdvanceRequest.cs
@@ -0,0 +1,26 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AlibabaCloud.SDK.Facebody20191230.Models
+{
+    public class RetouchBodyAdvanceRequest : TeaModel {
+        [NameInMap("ImageURLObject")]
+        [Validation(Required=true)]
+        public Stream ImageURLObject { get; set; }
+
+        [NameInMap("SlimDegree")]
+        [Validation(Required=false)]
+        public float? SlimDegree { get; set; }
+
+        [NameInMap("LengthenDegree")]
+        [Validation(Required=false)]
+        public float? LengthenDegree { get; set; }
+
+    }
+
+}
--- facebody-20191230/core/Properties/AssemblyInfo.cs
+++ facebody-20191230/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("21890e30-00ba-11ec-b329-0903b076de03")]
+[assembly: Guid("986a8fa0-04a8-11ec-a374-e3ded116b79e")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.11.0")]
-[assembly: AssemblyFileVersion("1.0.11.0")]
+[assembly: AssemblyVersion("1.0.12.0")]
+[assembly: AssemblyFileVersion("1.0.12.0")]
