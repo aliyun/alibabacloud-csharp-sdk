403984c94c8477dbb8af80b7650054b76a396563
sdk-team <sdk-team@alibabacloud.com>
2021-09-18 09:29:32 +0000

    Fix auto code generation problem.

--- dysmsapi-20180501/core/Properties/AssemblyInfo.cs
+++ dysmsapi-20180501/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ac2db550-185b-11ec-ac15-ef9f1ccefa67")]
+[assembly: Guid("e2ab46e0-1862-11ec-8db7-65718fa6e6d4")]
 
 // Version information for an assembly consists of the following four values:
 //
