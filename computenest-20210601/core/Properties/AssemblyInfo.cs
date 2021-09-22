661bb733a60af891c9fa5f9bc43d5c8248a8d618
sdk-team <sdk-team@alibabacloud.com>
2021-09-22 03:55:12 +0000

    ListServiceInstance support RequestTag parameter.

--- computenest-20210601/core/Properties/AssemblyInfo.cs
+++ computenest-20210601/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("66910440-1b56-11ec-bad7-71a76ba728ea")]
+[assembly: Guid("db20e1c0-1b58-11ec-b2dd-89e5d2d782de")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
