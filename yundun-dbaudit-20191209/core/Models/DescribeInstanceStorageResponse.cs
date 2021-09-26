3b14831202dee234cc5d8a149d46b9d85aed838d
sdk-team <sdk-team@alibabacloud.com>
2021-09-26 08:06:01 +0000

    Generated csharp 2019-12-09 for Yundun-dbaudit.

---
+++ yundun-dbaudit-20191209/core/Models/DescribeInstanceStorageResponse.cs
@@ -0,0 +1,22 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AlibabaCloud.SDK.Yundun_dbaudit20191209.Models
+{
+    public class DescribeInstanceStorageResponse : TeaModel {
+        [NameInMap("headers")]
+        [Validation(Required=true)]
+        public Dictionary<string, string> Headers { get; set; }
+
+        [NameInMap("body")]
+        [Validation(Required=true)]
+        public DescribeInstanceStorageResponseBody Body { get; set; }
+
+    }
+
+}
---
+++ yundun-dbaudit-20191209/core/Models/DescribeInstancesRequest.cs
@@ -0,0 +1,52 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AlibabaCloud.SDK.Yundun_dbaudit20191209.Models
+{
+    public class DescribeInstancesRequest : TeaModel {
+        [NameInMap("PageSize")]
+        [Validation(Required=false)]
+        public int? PageSize { get; set; }
+
+        [NameInMap("PageNumber")]
+        [Validation(Required=false)]
+        public int? PageNumber { get; set; }
+
+        [NameInMap("RegionId")]
+        [Validation(Required=false)]
+        public string RegionId { get; set; }
+
+        [NameInMap("InstanceStatus")]
+        [Validation(Required=false)]
+        public string InstanceStatus { get; set; }
+
+        [NameInMap("ResourceGroupId")]
+        [Validation(Required=false)]
+        public string ResourceGroupId { get; set; }
+
+        [NameInMap("InstanceId")]
+        [Validation(Required=false)]
+        public List<string> InstanceId { get; set; }
+
+        [NameInMap("Tag")]
+        [Validation(Required=false)]
+        public List<DescribeInstancesRequestTag> Tag { get; set; }
+        public class DescribeInstancesRequestTag : TeaModel {
+            [NameInMap("Key")]
+            [Validation(Required=false)]
+            public string Key { get; set; }
+
+            [NameInMap("Value")]
+            [Validation(Required=false)]
+            public string Value { get; set; }
+
+        }
+
+    }
+
+}
