1b8427c7713c543be2c1c6964986d0f21c22d333
sdk-team <sdk-team@alibabacloud.com>
2021-12-03 07:09:15 +0000

    Update fc pop sdk.

--- fc-open-20210406/core/Properties/AssemblyInfo.cs
+++ fc-open-20210406/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4ce5dab0-531a-11ec-b797-390235562352")]
+[assembly: Guid("e23437e0-5407-11ec-9415-0747510137ce")]
 
 // Version information for an assembly consists of the following four values:
 //
