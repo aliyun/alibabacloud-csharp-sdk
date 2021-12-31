18576fbf416045fc93c46c799113339fcf1296d6
sdk-team <sdk-team@alibabacloud.com>
2021-12-31 02:11:51 +0000

    Update ErrorCode.

--- dcdn-20180115/core/Properties/AssemblyInfo.cs
+++ dcdn-20180115/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("62144ce0-6851-11ec-85bd-9f1b443dcf1f")]
+[assembly: Guid("f3a8b9a0-69de-11ec-9aa9-b1cc37a5c496")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
