362adceaf23a19e4fc41dbaf35bbacee515b6563
sdk-team <sdk-team@alibabacloud.com>
2022-02-08 02:18:56 +0000

    Update SDK.

--- dyvmsapi-20170525/core/Properties/AssemblyInfo.cs
+++ dyvmsapi-20170525/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bb44d830-79b7-11ec-aea4-31293d8063bd")]
+[assembly: Guid("6c21b8e0-8885-11ec-acca-938793c088b3")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.1.1.0")]
-[assembly: AssemblyFileVersion("2.1.1.0")]
+[assembly: AssemblyVersion("2.1.2.0")]
+[assembly: AssemblyFileVersion("2.1.2.0")]
