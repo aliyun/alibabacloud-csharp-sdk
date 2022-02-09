d3b6e312010ddefaf7ae54d36eb9ae597f44b0ec
sdk-team <sdk-team@alibabacloud.com>
2022-02-09 06:22:53 +0000

    Support iotcc for iot.

--- iotcc-20210513/core/Properties/AssemblyInfo.cs
+++ iotcc-20210513/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ae2a67b0-7f3d-11ec-a476-63df695d1770")]
+[assembly: Guid("aae91ff0-8970-11ec-9424-7d012b049aff")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.4.0")]
-[assembly: AssemblyFileVersion("2.0.4.0")]
+[assembly: AssemblyVersion("2.0.5.0")]
+[assembly: AssemblyFileVersion("2.0.5.0")]
