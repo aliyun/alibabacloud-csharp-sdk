2117dee1bdf0437d3bd409b45062828d0f2a8315
sdk-team <sdk-team@alibabacloud.com>
2022-02-22 08:39:52 +0000

    New build from amp.

--- servicemesh-20200111/core/Properties/AssemblyInfo.cs
+++ servicemesh-20200111/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("af0113b0-8fc7-11ec-be58-250e61b701a5")]
+[assembly: Guid("f3d2c780-93ba-11ec-86ff-0182a3886a0c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.5.0")]
-[assembly: AssemblyFileVersion("2.0.5.0")]
+[assembly: AssemblyVersion("2.0.6.0")]
+[assembly: AssemblyFileVersion("2.0.6.0")]
