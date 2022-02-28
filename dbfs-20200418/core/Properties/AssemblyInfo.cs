8c093258f33a587fa9769af84ec75868811ff854
sdk-team <sdk-team@alibabacloud.com>
2022-02-28 09:31:55 +0000

    Fixed the default performance level of CreateDbfs to be PL1.

--- dbfs-20200418/core/Properties/AssemblyInfo.cs
+++ dbfs-20200418/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9cb17110-8a3f-11ec-9156-1fd070ccbfbf")]
+[assembly: Guid("3ab9c280-9879-11ec-b838-95ece79184ce")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.7.0")]
-[assembly: AssemblyFileVersion("3.0.7.0")]
+[assembly: AssemblyVersion("3.0.8.0")]
+[assembly: AssemblyFileVersion("3.0.8.0")]
