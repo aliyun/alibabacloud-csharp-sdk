fb06d4afed47a2f2f3f999dce082878ff2d87ee8
sdk-team <sdk-team@alibabacloud.com>
2022-02-28 11:25:12 +0000

    Support DescribeSuspEvents, DescribeAlarmEventList API.

--- sas-20181203/core/Properties/AssemblyInfo.cs
+++ sas-20181203/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("32def1b0-9604-11ec-beee-3fe41233005d")]
+[assembly: Guid("06239720-9889-11ec-9860-9d81ed03caea")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.13.0")]
-[assembly: AssemblyFileVersion("1.0.13.0")]
+[assembly: AssemblyVersion("1.0.14.0")]
+[assembly: AssemblyFileVersion("1.0.14.0")]
