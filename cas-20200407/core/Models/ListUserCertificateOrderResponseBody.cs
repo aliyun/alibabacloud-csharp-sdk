4b5d1a7ce3cb83ee766088c67b6d4e25e38e90fe
sdk-team <sdk-team@alibabacloud.com>
2022-03-16 11:30:51 +0000

    Fixed ListUserCertificateOrder API response error bug.

--- cas-20200407/core/Models/ListUserCertificateOrderResponseBody.cs
+++ cas-20200407/core/Models/ListUserCertificateOrderResponseBody.cs
@@ -11,7 +11,85 @@ namespace AlibabaCloud.SDK.Cas20200407.Models
     public class ListUserCertificateOrderResponseBody : TeaModel {
         [NameInMap("CertificateOrderList")]
         [Validation(Required=false)]
-        public string CertificateOrderList { get; set; }
+        public List<ListUserCertificateOrderResponseBodyCertificateOrderList> CertificateOrderList { get; set; }
+        public class ListUserCertificateOrderResponseBodyCertificateOrderList : TeaModel {
+            [NameInMap("Algorithm")]
+            [Validation(Required=false)]
+            public string Algorithm { get; set; }
+
+            [NameInMap("AliyunOrderId")]
+            [Validation(Required=false)]
+            public long? AliyunOrderId { get; set; }
+
+            [NameInMap("BuyDate")]
+            [Validation(Required=false)]
+            public long? BuyDate { get; set; }
+
+            [NameInMap("CertEndTime")]
+            [Validation(Required=false)]
+            public long? CertEndTime { get; set; }
+
+            [NameInMap("CertStartTime")]
+            [Validation(Required=false)]
+            public long? CertStartTime { get; set; }
+
+            [NameInMap("CertType")]
+            [Validation(Required=false)]
+            public string CertType { get; set; }
+
+            [NameInMap("Domain")]
+            [Validation(Required=false)]
+            public string Domain { get; set; }
+
+            [NameInMap("DomainCount")]
+            [Validation(Required=false)]
+            public long? DomainCount { get; set; }
+
+            [NameInMap("DomainType")]
+            [Validation(Required=false)]
+            public string DomainType { get; set; }
+
+            [NameInMap("InstanceId")]
+            [Validation(Required=false)]
+            public string InstanceId { get; set; }
+
+            [NameInMap("OrderId")]
+            [Validation(Required=false)]
+            public long? OrderId { get; set; }
+
+            [NameInMap("PartnerOrderId")]
+            [Validation(Required=false)]
+            public string PartnerOrderId { get; set; }
+
+            [NameInMap("ProductCode")]
+            [Validation(Required=false)]
+            public string ProductCode { get; set; }
+
+            [NameInMap("ProductName")]
+            [Validation(Required=false)]
+            public string ProductName { get; set; }
+
+            [NameInMap("RootBrand")]
+            [Validation(Required=false)]
+            public string RootBrand { get; set; }
+
+            [NameInMap("SourceType")]
+            [Validation(Required=false)]
+            public string SourceType { get; set; }
+
+            [NameInMap("Status")]
+            [Validation(Required=false)]
+            public string Status { get; set; }
+
+            [NameInMap("TrusteeStatus")]
+            [Validation(Required=false)]
+            public string TrusteeStatus { get; set; }
+
+            [NameInMap("WildDomainCount")]
+            [Validation(Required=false)]
+            public long? WildDomainCount { get; set; }
+
+        }
 
         [NameInMap("CurrentPage")]
         [Validation(Required=false)]
--- cas-20200407/core/Properties/AssemblyInfo.cs
+++ cas-20200407/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cafcd920-a514-11ec-a96b-0f561c69f507")]
+[assembly: Guid("7fd6e5a0-a51c-11ec-bb56-4980b6a4f540")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
