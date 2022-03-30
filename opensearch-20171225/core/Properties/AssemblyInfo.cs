a75f9ce0b02fa6dd624d75307b8cfe64dd59c6e0
sdk-team <sdk-team@alibabacloud.com>
2022-03-30 02:36:24 +0000

    Update algorithm service interface.

--- opensearch-20171225/core/Properties/AssemblyInfo.cs
+++ opensearch-20171225/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("71f26370-af39-11ec-8f2a-8370bd0e6312")]
+[assembly: Guid("22842000-afd2-11ec-bbc3-dfa0f794485a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
