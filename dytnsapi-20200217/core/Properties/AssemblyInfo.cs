6cebdbf93d8c3c3d16197bf8bda0f7681ddb3d45
sdk-team <sdk-team@alibabacloud.com>
2022-04-07 09:01:10 +0000

    Supported New Features.

--- dytnsapi-20200217/core/Properties/AssemblyInfo.cs
+++ dytnsapi-20200217/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b087a9e0-a8c4-11ec-bc16-9d05455eb78a")]
+[assembly: Guid("3b9046c0-b651-11ec-840b-bda6ad74cf9b")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.7.0")]
-[assembly: AssemblyFileVersion("1.0.7.0")]
+[assembly: AssemblyVersion("1.0.8.0")]
+[assembly: AssemblyFileVersion("1.0.8.0")]
