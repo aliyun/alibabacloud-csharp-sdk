e6ea50588e3161dfc6a3ac9356e4e5d10bf899d0
sdk-team <sdk-team@alibabacloud.com>
2022-04-08 13:41:43 +0000

    Release FeedbackSession.

--- imageprocess-20200320/core/Models/FeedbackSessionResponseBody.cs
+++ imageprocess-20200320/core/Models/FeedbackSessionResponseBody.cs
@@ -9,15 +9,6 @@ using Tea;
 namespace AlibabaCloud.SDK.Imageprocess20200320.Models
 {
     public class FeedbackSessionResponseBody : TeaModel {
-        [NameInMap("Data")]
-        [Validation(Required=false)]
-        public FeedbackSessionResponseBodyData Data { get; set; }
-        public class FeedbackSessionResponseBodyData : TeaModel {
-            [NameInMap("Data")]
-            [Validation(Required=false)]
-            public string Data { get; set; }
-        };
-
         /// <summary>
         /// Id of the request
         /// </summary>
