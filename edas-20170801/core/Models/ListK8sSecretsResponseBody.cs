ab9b670af0f31f4499888db622f1be88397ba675
sdk-team <sdk-team@alibabacloud.com>
2022-04-13 03:03:13 +0000

    Supported ConfigMap management for K8s Applications.

--- edas-20170801/core/Models/ListK8sSecretsResponseBody.cs
+++ edas-20170801/core/Models/ListK8sSecretsResponseBody.cs
@@ -23,135 +23,69 @@ namespace AlibabaCloud.SDK.Edas20170801.Models
 
         [NameInMap("Result")]
         [Validation(Required=false)]
-        public List<ListK8sSecretsResponseBodyResult> Result { get; set; }
+        public ListK8sSecretsResponseBodyResult Result { get; set; }
         public class ListK8sSecretsResponseBodyResult : TeaModel {
             [NameInMap("Secrets")]
             [Validation(Required=false)]
             public List<ListK8sSecretsResponseBodyResultSecrets> Secrets { get; set; }
             public class ListK8sSecretsResponseBodyResultSecrets : TeaModel {
-                [NameInMap("Base64Encoded")]
-                [Validation(Required=false)]
                 public bool? Base64Encoded { get; set; }
-
-                [NameInMap("CertDetail")]
-                [Validation(Required=false)]
                 public ListK8sSecretsResponseBodyResultSecretsCertDetail CertDetail { get; set; }
                 public class ListK8sSecretsResponseBodyResultSecretsCertDetail : TeaModel {
                     [NameInMap("DomainNames")]
                     [Validation(Required=false)]
                     public List<string> DomainNames { get; set; }
+
                     [NameInMap("EndTime")]
                     [Validation(Required=false)]
                     public string EndTime { get; set; }
+
                     [NameInMap("Issuer")]
                     [Validation(Required=false)]
                     public string Issuer { get; set; }
-                    [NameInMap("KeySize")]
-                    [Validation(Required=false)]
-                    public int? KeySize { get; set; }
+
                     [NameInMap("StartTime")]
                     [Validation(Required=false)]
                     public string StartTime { get; set; }
+
                     [NameInMap("Status")]
                     [Validation(Required=false)]
                     public string Status { get; set; }
-                };
 
-                [NameInMap("CertId")]
-                [Validation(Required=false)]
+                }
                 public string CertId { get; set; }
-
-                [NameInMap("CertRegionId")]
-                [Validation(Required=false)]
                 public string CertRegionId { get; set; }
-
-                [NameInMap("ClusterId")]
-                [Validation(Required=false)]
                 public string ClusterId { get; set; }
-
-                [NameInMap("ClusterName")]
-                [Validation(Required=false)]
                 public string ClusterName { get; set; }
-
-                [NameInMap("CreationTime")]
-                [Validation(Required=false)]
                 public string CreationTime { get; set; }
-
-                [NameInMap("Data")]
-                [Validation(Required=false)]
                 public List<ListK8sSecretsResponseBodyResultSecretsData> Data { get; set; }
                 public class ListK8sSecretsResponseBodyResultSecretsData : TeaModel {
-                    [NameInMap("Key")]
-                    [Validation(Required=false)]
                     public string Key { get; set; }
-
-                    [NameInMap("Value")]
-                    [Validation(Required=false)]
                     public string Value { get; set; }
-
                 }
-
-                [NameInMap("Name")]
-                [Validation(Required=false)]
                 public string Name { get; set; }
-
-                [NameInMap("Namespace")]
-                [Validation(Required=false)]
                 public string Namespace { get; set; }
-
-                [NameInMap("RelatedApps")]
-                [Validation(Required=false)]
                 public List<ListK8sSecretsResponseBodyResultSecretsRelatedApps> RelatedApps { get; set; }
                 public class ListK8sSecretsResponseBodyResultSecretsRelatedApps : TeaModel {
-                    [NameInMap("AppId")]
-                    [Validation(Required=false)]
                     public string AppId { get; set; }
-
-                    [NameInMap("AppName")]
-                    [Validation(Required=false)]
                     public string AppName { get; set; }
-
                 }
-
-                [NameInMap("RelatedIngressRules")]
-                [Validation(Required=false)]
                 public List<ListK8sSecretsResponseBodyResultSecretsRelatedIngressRules> RelatedIngressRules { get; set; }
                 public class ListK8sSecretsResponseBodyResultSecretsRelatedIngressRules : TeaModel {
-                    [NameInMap("Name")]
-                    [Validation(Required=false)]
                     public string Name { get; set; }
-
-                    [NameInMap("Namespace")]
-                    [Validation(Required=false)]
                     public string Namespace { get; set; }
-
-                    [NameInMap("RelatedApps")]
-                    [Validation(Required=false)]
                     public List<ListK8sSecretsResponseBodyResultSecretsRelatedIngressRulesRelatedApps> RelatedApps { get; set; }
                     public class ListK8sSecretsResponseBodyResultSecretsRelatedIngressRulesRelatedApps : TeaModel {
-                        [NameInMap("AppId")]
-                        [Validation(Required=false)]
                         public string AppId { get; set; }
-
-                        [NameInMap("AppName")]
-                        [Validation(Required=false)]
                         public string AppName { get; set; }
-
                     }
-
                 }
-
-                [NameInMap("Type")]
-                [Validation(Required=false)]
                 public string Type { get; set; }
-
             }
-
             [NameInMap("Total")]
             [Validation(Required=false)]
             public int? Total { get; set; }
-
-        }
+        };
 
     }
 
--- edas-20170801/core/Properties/AssemblyInfo.cs
+++ edas-20170801/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("40561210-ba08-11ec-a37c-51ed3034daac")]
+[assembly: Guid("315933e0-bad6-11ec-a36b-3144cd245b68")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
