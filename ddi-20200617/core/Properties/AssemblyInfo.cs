0e787b09fa232b81a2d7bdc008913b4599772c1f
sdk-team <sdk-team@alibabacloud.com>
2022-04-25 11:13:58 +0000

    Add flow API.

--- ddi-20200617/core/Properties/AssemblyInfo.cs
+++ ddi-20200617/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6073c2a0-af26-11ec-9455-9feeac2f8699")]
+[assembly: Guid("c2f5d590-c488-11ec-88a9-c18717a4e44b")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.9.0")]
-[assembly: AssemblyFileVersion("1.0.9.0")]
+[assembly: AssemblyVersion("1.0.10.0")]
+[assembly: AssemblyFileVersion("1.0.10.0")]
