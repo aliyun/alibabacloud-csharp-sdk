01b437ea998dc7bb8b3a470b4f3e909a9e9a1f51
sdk-team <sdk-team@alibabacloud.com>
2022-05-06 08:50:20 +0000

    Iot_20180120 TeaDSL sdk.

--- iot-20180120/core/Properties/AssemblyInfo.cs
+++ iot-20180120/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8c5279c0-cd17-11ec-9406-95ec47938591")]
+[assembly: Guid("77e0f230-cd19-11ec-9903-e95a753b1cb8")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.3.0")]
-[assembly: AssemblyFileVersion("3.0.3.0")]
+[assembly: AssemblyVersion("3.0.4.0")]
+[assembly: AssemblyFileVersion("3.0.4.0")]
