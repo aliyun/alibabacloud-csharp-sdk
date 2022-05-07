d830b261c2edd6b90d5c6ffa434b234391941666
sdk-team <sdk-team@alibabacloud.com>
2022-05-07 09:43:51 +0000

    Added diagnostic feature interface.

--- ros-20190910/core/Properties/AssemblyInfo.cs
+++ ros-20190910/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ee449b40-c489-11ec-82a9-c115c8b84782")]
+[assembly: Guid("274298c0-cdea-11ec-8b6e-d3b49d5b0f8d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.2.1.0")]
-[assembly: AssemblyFileVersion("2.2.1.0")]
+[assembly: AssemblyVersion("2.2.2.0")]
+[assembly: AssemblyFileVersion("2.2.2.0")]
