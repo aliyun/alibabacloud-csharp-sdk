0373bf5918c41c7daa2274ce14241fe2bc76ea6d
sdk-team <sdk-team@alibabacloud.com>
2022-05-27 10:09:04 +0000

    Add describe dbsc for ecs api.

--- ebs-20210730/core/Models/DescribeDiskReplicaPairsRequest.cs
+++ ebs-20210730/core/Models/DescribeDiskReplicaPairsRequest.cs
@@ -9,14 +9,27 @@ using Tea;
 namespace AlibabaCloud.SDK.Ebs20210730.Models
 {
     public class DescribeDiskReplicaPairsRequest : TeaModel {
+        /// <summary>
+        /// 分页查询时每页的最大条目数。取值范围：1~500
+        /// 
+        /// 默认值：10
+        /// </summary>
         [NameInMap("MaxResults")]
         [Validation(Required=false)]
         public long? MaxResults { get; set; }
 
+        /// <summary>
+        /// 查询凭证（Token）。取值为上一次调用该接口返回的NextToken参数值，初次调用接口时无需设置该参数。
+        /// </summary>
         [NameInMap("NextToken")]
         [Validation(Required=false)]
         public string NextToken { get; set; }
 
+        /// <summary>
+        /// 异步复制关系ID列表。您可以指定一个或多个异步复制关系ID进行查询。格式为：pair-cn-dsa****,pair-cn-asd****。
+        /// 
+        /// 默认值为空，表示查询当前地域下所有的异步复制关系。
+        /// </summary>
         [NameInMap("PairIds")]
         [Validation(Required=false)]
         public string PairIds { get; set; }
@@ -25,12 +38,15 @@ namespace AlibabaCloud.SDK.Ebs20210730.Models
         [Validation(Required=false)]
         public string RegionId { get; set; }
 
+        /// <summary>
+        /// 所属复制组id。
+        /// </summary>
         [NameInMap("ReplicaGroupId")]
         [Validation(Required=false)]
         public string ReplicaGroupId { get; set; }
 
         /// <summary>
-        /// production或backup，表示数据从主或备站点获取，默认为production。
+        /// production或backup，表示获取本地为主站点或备站点的复制对数据，默认为production。
         /// </summary>
         [NameInMap("Site")]
         [Validation(Required=false)]
--- ebs-20210730/core/Properties/AssemblyInfo.cs
+++ ebs-20210730/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c8115530-ba4d-11ec-8555-d72abe0818d8")]
+[assembly: Guid("00204560-dda5-11ec-9a3a-d3d4bffcc823")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
