588f4109e6f31300fae4ec1389f2653042779113
sdk-team <sdk-team@alibabacloud.com>
2022-07-04 12:24:43 +0000

    Update API error code.

--- quickbi-public-20220101/core/Properties/AssemblyInfo.cs
+++ quickbi-public-20220101/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("97c783f0-a37c-11ec-abea-438884761643")]
+[assembly: Guid("3ce03160-fb94-11ec-a6cc-ef57e153ac94")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
