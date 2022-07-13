85f0b5efee71ae3cee1a46f38c062d9e26c2d6b2
sdk-team <sdk-team@alibabacloud.com>
2022-07-13 04:16:32 +0000

    Automatically generate sdk tasks.

--- wfts-20220212/core/Properties/AssemblyInfo.cs
+++ wfts-20220212/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d415dfc0-0261-11ed-99a5-7b69c2f7d11a")]
+[assembly: Guid("8b7b7d50-0262-11ed-90d0-13bd648ded2f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.7.0")]
-[assembly: AssemblyFileVersion("1.0.7.0")]
+[assembly: AssemblyVersion("1.0.8.0")]
+[assembly: AssemblyFileVersion("1.0.8.0")]
