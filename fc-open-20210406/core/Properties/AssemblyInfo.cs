eecdaeef50b537b69423385ee95e008adc81e710
sdk-team <sdk-team@alibabacloud.com>
2022-07-18 03:34:47 +0000

    Fix InvokeFunction serilization.

--- fc-open-20210406/core/Properties/AssemblyInfo.cs
+++ fc-open-20210406/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8384d180-fc47-11ec-a15e-13863ae7cd4b")]
+[assembly: Guid("85554d20-064a-11ed-a21b-933bf0225b5e")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.5.0")]
-[assembly: AssemblyFileVersion("1.1.5.0")]
+[assembly: AssemblyVersion("1.1.6.0")]
+[assembly: AssemblyFileVersion("1.1.6.0")]
