f5e33e3fd1d101eb5cc174465d75d3146e4e0c68
sdk-team <sdk-team@alibabacloud.com>
2022-07-18 09:48:47 +0000

    Change the parameter tag to optional.

--- cloudapi-20160714/core/Properties/AssemblyInfo.cs
+++ cloudapi-20160714/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5a18e7e0-03e1-11ed-a138-b169941c9e99")]
+[assembly: Guid("c31a9aa0-067e-11ed-b081-3d47ef35fc7d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.2.1.0")]
-[assembly: AssemblyFileVersion("2.2.1.0")]
+[assembly: AssemblyVersion("2.2.3.0")]
+[assembly: AssemblyFileVersion("2.2.3.0")]
