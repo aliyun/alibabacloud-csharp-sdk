0243c5ccc748f850f0d54a5b5c5833ec06f74a37
sdk-team <sdk-team@alibabacloud.com>
2022-07-19 10:56:15 +0000

    Update by SDK platform.

--- dds-20151201/core/Properties/AssemblyInfo.cs
+++ dds-20151201/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e535f360-f61f-11ec-84ce-770e1e1f091c")]
+[assembly: Guid("5a9c46d0-0751-11ed-820e-53af5d4b6eca")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.6.7.0")]
-[assembly: AssemblyFileVersion("3.6.7.0")]
+[assembly: AssemblyVersion("3.6.8.0")]
+[assembly: AssemblyFileVersion("3.6.8.0")]
