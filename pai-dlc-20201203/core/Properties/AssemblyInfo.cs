d4de4c8720aa9927d431f3568bdd954f048e071d
sdk-team <sdk-team@alibabacloud.com>
2022-07-26 09:58:20 +0000

    Support to automatically fill in endpoint according to region.

--- pai-dlc-20201203/core/Properties/AssemblyInfo.cs
+++ pai-dlc-20201203/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c60fd590-0caf-11ed-83bc-b982926c618e")]
+[assembly: Guid("70cf7f80-0cc9-11ed-961e-91710b930a70")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.5.0")]
-[assembly: AssemblyFileVersion("1.1.5.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
