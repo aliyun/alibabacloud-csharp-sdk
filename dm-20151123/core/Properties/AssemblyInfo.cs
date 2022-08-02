02c5bedc49261042d690df1a888510582adbcf00
sdk-team <sdk-team@alibabacloud.com>
2022-08-02 09:24:44 +0000

    The input parameter IpProtection of UpdateIpProtection API  is corrected to be required.

--- dm-20151123/core/Properties/AssemblyInfo.cs
+++ dm-20151123/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("13600190-d7ee-11ec-8d02-6d8e1ec56563")]
+[assembly: Guid("e7a63920-1244-11ed-a4bb-33084268e553")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
