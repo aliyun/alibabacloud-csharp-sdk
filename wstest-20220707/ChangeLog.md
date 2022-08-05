13354a17119350523c972f93db9a49c1a3c89842
sdk-team <sdk-team@alibabacloud.com>
2022-08-05 02:19:15 +0000

    Generated csharp 2022-07-07 for wsTest.

---
+++ wstest-20220707/ChangeLog.md
@@ -0,0 +1,3 @@
+2022-08-05 Version: 1.0.0
+- Generated csharp 2022-07-07 for wsTest.
+
---
+++ wstest-20220707/core/Models/TestPopHsfFailedRequest.cs
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AlibabaCloud.SDK.WsTest20220707.Models
+{
+    public class TestPopHsfFailedRequest : TeaModel {
+        [NameInMap("Name")]
+        [Validation(Required=false)]
+        public string Name { get; set; }
+
+    }
+
+}
