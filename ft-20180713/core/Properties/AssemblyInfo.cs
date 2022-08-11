2b35e3e90503f6bf505770558935198826199b51
sdk-team <sdk-team@alibabacloud.com>
2022-08-11 11:47:45 +0000

    AMP Version Change.

--- ft-20180713/core/Properties/AssemblyInfo.cs
+++ ft-20180713/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b55ccec0-1316-11ed-98c8-27a702020a7b")]
+[assembly: Guid("38fee4d0-196a-11ed-b70e-a3617ab6a5dc")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
