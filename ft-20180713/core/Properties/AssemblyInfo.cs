bc87ed57121b0c03f3a57dc7359200db83a0914b
sdk-team <sdk-team@alibabacloud.com>
2022-08-11 11:45:40 +0000

    [WIP]mocktest变成待审核，请勿合并

--- ft-20180713/core/Properties/AssemblyInfo.cs
+++ ft-20180713/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b55ccec0-1316-11ed-98c8-27a702020a7b")]
+[assembly: Guid("1da2ff90-196b-11ed-9bea-bbc9e7553e39")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
