e14d5c8cd2b48a128a52bfe1e3c174fa84897665
sdk-team <sdk-team@alibabacloud.com>
2022-08-12 02:53:12 +0000

    Fix ListManagedRules bugs.

--- config-20200907/core/Properties/AssemblyInfo.cs
+++ config-20200907/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e835b2d0-19e2-11ed-9057-d1a3e08a024a")]
+[assembly: Guid("bfa1cdc0-19e9-11ed-abc6-a7e185bde72e")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.1.5.0")]
-[assembly: AssemblyFileVersion("2.1.5.0")]
+[assembly: AssemblyVersion("2.1.7.0")]
+[assembly: AssemblyFileVersion("2.1.7.0")]
