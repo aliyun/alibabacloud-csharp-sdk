3e6854c6cec9625fb9677164d022d50c355e7195
sdk-team <sdk-team@alibabacloud.com>
2022-08-26 07:37:03 +0000

    Add some api.

--- btripopen-20220520/core/Properties/AssemblyInfo.cs
+++ btripopen-20220520/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cab43880-24e6-11ed-adf0-33700ee1d2ec")]
+[assembly: Guid("a1714460-2511-11ed-b096-917745c8fe25")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.7.0")]
-[assembly: AssemblyFileVersion("1.0.7.0")]
+[assembly: AssemblyVersion("1.0.8.0")]
+[assembly: AssemblyFileVersion("1.0.8.0")]
