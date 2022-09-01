2e1c65733b93dbe4fd5784e42bef80776ec66dbb
sdk-team <sdk-team@alibabacloud.com>
2022-09-01 08:45:39 +0000

    Generated csharp 2021-05-15 for cloudsso.

--- cloudsso-20210515/core/Properties/AssemblyInfo.cs
+++ cloudsso-20210515/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ff165990-29ce-11ed-9883-5d9b0006213c")]
+[assembly: Guid("64e0d2c0-29d2-11ed-867f-ab3101d6a858")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
