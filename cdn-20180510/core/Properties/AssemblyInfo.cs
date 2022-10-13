a3c751473fb7384d4b1599e1bb3d89618e852f30
sdk-team <sdk-team@alibabacloud.com>
2022-10-13 09:08:46 +0000

    Update DetachDomainNameFromVersion.

--- cdn-20180510/core/Properties/AssemblyInfo.cs
+++ cdn-20180510/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1f17ef70-3ed2-11ed-8004-2b090b76d342")]
+[assembly: Guid("73596a90-4ad6-11ed-93f9-33aad389d405")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.2.0")]
-[assembly: AssemblyFileVersion("1.1.2.0")]
+[assembly: AssemblyVersion("1.1.3.0")]
+[assembly: AssemblyFileVersion("1.1.3.0")]
