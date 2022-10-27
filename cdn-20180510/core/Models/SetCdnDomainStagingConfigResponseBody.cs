257df130dc2290bed3c9bec6cffa16f216721d04
sdk-team <sdk-team@alibabacloud.com>
2022-10-27 10:23:47 +0000

    Add DescribeWildcardDomainDetailData.

--- cdn-20180510/core/Models/SetCdnDomainStagingConfigResponseBody.cs
+++ cdn-20180510/core/Models/SetCdnDomainStagingConfigResponseBody.cs
@@ -9,6 +9,24 @@ using Tea;
 namespace AlibabaCloud.SDK.Cdn20180510.Models
 {
     public class SetCdnDomainStagingConfigResponseBody : TeaModel {
+        [NameInMap("DomainConfigList")]
+        [Validation(Required=false)]
+        public List<SetCdnDomainStagingConfigResponseBodyDomainConfigList> DomainConfigList { get; set; }
+        public class SetCdnDomainStagingConfigResponseBodyDomainConfigList : TeaModel {
+            [NameInMap("ConfigId")]
+            [Validation(Required=false)]
+            public long? ConfigId { get; set; }
+
+            [NameInMap("DomainName")]
+            [Validation(Required=false)]
+            public string DomainName { get; set; }
+
+            [NameInMap("FunctionName")]
+            [Validation(Required=false)]
+            public string FunctionName { get; set; }
+
+        }
+
         [NameInMap("RequestId")]
         [Validation(Required=false)]
         public string RequestId { get; set; }
--- cdn-20180510/core/Properties/AssemblyInfo.cs
+++ cdn-20180510/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("73596a90-4ad6-11ed-93f9-33aad389d405")]
+[assembly: Guid("51d353c0-55e1-11ed-b039-554487f9f87a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.3.0")]
-[assembly: AssemblyFileVersion("1.1.3.0")]
+[assembly: AssemblyVersion("1.1.4.0")]
+[assembly: AssemblyFileVersion("1.1.4.0")]
