ff7b69e258f8d0acf94f423bdc4cbdb191072bd2
sdk-team <sdk-team@alibabacloud.com>
2022-10-31 14:22:18 +0000

    Add CreateWorkitemComment and change some API params.

--- devops-20210625/core/Properties/AssemblyInfo.cs
+++ devops-20210625/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("93fb2b30-578d-11ed-9396-81c0cad52da6")]
+[assembly: Guid("4eef0fc0-5927-11ed-8cf7-7d7f5e55eb5a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.19.0")]
-[assembly: AssemblyFileVersion("1.1.19.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
