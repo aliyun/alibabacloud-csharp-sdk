755e3f4fd1589bad864d616f47116381a1422477
sdk-team <sdk-team@alibabacloud.com>
2022-11-07 06:52:04 +0000

    Cc5g.

--- cc5g-20220314/core/Properties/AssemblyInfo.cs
+++ cc5g-20220314/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("253cb010-58e6-11ed-b205-ab24b466e2cd")]
+[assembly: Guid("8ce040b0-5e68-11ed-a300-2108c7cd8525")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.3.0")]
-[assembly: AssemblyFileVersion("2.0.3.0")]
+[assembly: AssemblyVersion("2.0.4.0")]
+[assembly: AssemblyFileVersion("2.0.4.0")]
