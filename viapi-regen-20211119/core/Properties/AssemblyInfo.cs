9572343e134817e38461595ac99caf6fea80bca9
sdk-team <sdk-team@alibabacloud.com>
2022-11-24 03:38:15 +0000

    Release CustomizeClassifyImage.

--- viapi-regen-20211119/core/Properties/AssemblyInfo.cs
+++ viapi-regen-20211119/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d47f0700-6186-11ed-983b-bdd86ebbb775")]
+[assembly: Guid("5ae9f300-6ba9-11ed-9c9e-b7294f75f5ad")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.4.0")]
-[assembly: AssemblyFileVersion("1.0.4.0")]
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
