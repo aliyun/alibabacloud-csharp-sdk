4512cded567603b82ebceb33b7370469e12b5d42
sdk-team <sdk-team@alibabacloud.com>
2022-12-02 06:58:02 +0000

    Add OpenAPI as below, ListClusters, TagResources, UntagResources, ListTagResources.

--- emr-20210320/core/Properties/AssemblyInfo.cs
+++ emr-20210320/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4afac780-7207-11ed-ae19-a5eb78ed94e2")]
+[assembly: Guid("8bf87d20-720e-11ed-9899-518b88122872")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
