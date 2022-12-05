9dd9adb82ff19b58af490852efbb16c289fba68b
sdk-team <sdk-team@alibabacloud.com>
2022-12-05 03:08:34 +0000

    Live sdk update.

--- live-20161101/core/Properties/AssemblyInfo.cs
+++ live-20161101/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8259f660-7446-11ed-a64e-f34c78ed1f3f")]
+[assembly: Guid("ee739420-7449-11ed-a50a-0ff693379fd5")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
