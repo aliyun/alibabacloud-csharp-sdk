aa432dcd96b55d66724368934ea396b05f95bb9d
sdk-team <sdk-team@alibabacloud.com>
2022-12-20 03:26:38 +0000

    Support ent verify.

--- cloudauth-20221125/core/Properties/AssemblyInfo.cs
+++ cloudauth-20221125/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("98c8b2a0-7c49-11ed-ab8b-650509bb52b7")]
+[assembly: Guid("05871c90-8016-11ed-b546-adf581b492ac")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
