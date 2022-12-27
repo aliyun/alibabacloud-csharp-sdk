2d835b0af208f453daf49d4acc8a73275f196f33
sdk-team <sdk-team@alibabacloud.com>
2022-12-27 06:05:30 +0000

    Support Sms Verification for Dypnsapi.

--- dypnsapi-20170525/core/Properties/AssemblyInfo.cs
+++ dypnsapi-20170525/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("de681a60-705c-11ed-a336-4fe39842888b")]
+[assembly: Guid("5d07e120-85ac-11ed-b714-437585fb2703")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.6.0")]
-[assembly: AssemblyFileVersion("1.0.6.0")]
+[assembly: AssemblyVersion("1.0.7.0")]
+[assembly: AssemblyFileVersion("1.0.7.0")]
