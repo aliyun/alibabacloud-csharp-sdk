507ad09e477b9cabc1712c9a1b4c56300c21ab10
sdk-team <sdk-team@alibabacloud.com>
2023-01-05 09:27:34 +0000

    Modify some interfaces.

--- qssj-20220112/core/Properties/AssemblyInfo.cs
+++ qssj-20220112/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("10a88840-8cd0-11ed-852e-d9bef85bddba")]
+[assembly: Guid("11ee9bd0-8cdb-11ed-a8b8-5d6bc9f4b3ef")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.0")]
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
