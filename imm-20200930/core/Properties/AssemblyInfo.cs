bfc8fc587dbd8b6d798bb07f151b4c272b6fe693
sdk-team <sdk-team@alibabacloud.com>
2023-01-09 05:50:46 +0000

    Update UpdateFigureCluster Response.

--- imm-20200930/core/Properties/AssemblyInfo.cs
+++ imm-20200930/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c5eecd20-7f7b-11ed-8c08-b76812515b1a")]
+[assembly: Guid("74796600-8fe1-11ed-81b8-3f6544d2fe32")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.29.1.0")]
-[assembly: AssemblyFileVersion("1.29.1.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
