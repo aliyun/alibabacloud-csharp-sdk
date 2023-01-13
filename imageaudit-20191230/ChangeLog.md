4f6fb1cc7f3b435d3ea880a1d865434aac915a7d
sdk-team <sdk-team@alibabacloud.com>
2023-01-13 06:07:48 +0000

    Update imageaudit.

--- imageaudit-20191230/ChangeLog.md
+++ imageaudit-20191230/ChangeLog.md
@@ -1,3 +1,6 @@
+2023-01-13 Version: 1.0.5
+- Update imageaudit.
+
 2023-01-11 Version: 1.0.4
 - Update imageaudit.
 
--- imageaudit-20191230/core/Properties/AssemblyInfo.cs
+++ imageaudit-20191230/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e8630100-915a-11ed-ab52-2beeced996f9")]
+[assembly: Guid("823cfd90-9308-11ed-983c-f5586ebf2f90")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
