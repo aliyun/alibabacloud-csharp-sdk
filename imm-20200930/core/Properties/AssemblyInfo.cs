3ee3836dfe67e410c3f033f041a34567e1177eec
sdk-team <sdk-team@alibabacloud.com>
2023-01-16 04:21:38 +0000

    Remove deprecated APIs.

--- imm-20200930/core/Properties/AssemblyInfo.cs
+++ imm-20200930/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("74796600-8fe1-11ed-81b8-3f6544d2fe32")]
+[assembly: Guid("2ae80180-9555-11ed-9f31-83c016bc1ef7")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.1.1.0")]
+[assembly: AssemblyFileVersion("2.1.1.0")]
