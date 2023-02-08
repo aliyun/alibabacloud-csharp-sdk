d52c259ee972dfdad93107734452fb2a3496e205
sdk-team <sdk-team@alibabacloud.com>
2023-02-08 10:06:50 +0000

    Support SimilarImageClustering.

--- imm-20200930/core/Properties/AssemblyInfo.cs
+++ imm-20200930/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("dc162ae0-a761-11ed-a3b6-831315877e89")]
+[assembly: Guid("2ca87ae0-a798-11ed-a3c5-1d966d7c1133")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.1.4.0")]
-[assembly: AssemblyFileVersion("2.1.4.0")]
+[assembly: AssemblyVersion("2.1.5.0")]
+[assembly: AssemblyFileVersion("2.1.5.0")]
