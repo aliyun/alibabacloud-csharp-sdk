f070be3729f090f3d9a06f99af9d7886999453d1
sdk-team <sdk-team@alibabacloud.com>
2023-02-17 12:28:37 +0000

    Support for deploy.

--- viapi-20230117/core/Properties/AssemblyInfo.cs
+++ viapi-20230117/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("22b3bab0-adea-11ed-bee2-2f756aaae773")]
+[assembly: Guid("7badcb40-aebe-11ed-8657-3bdb2b0fe84b")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
