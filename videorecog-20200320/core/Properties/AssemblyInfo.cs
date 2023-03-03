bdb24e7f219bda0ea2e90d4a990a6e65566d54e4
sdk-team <sdk-team@alibabacloud.com>
2023-03-03 05:35:58 +0000

    Update EvaluateVideoQuality.

--- videorecog-20200320/core/Properties/AssemblyInfo.cs
+++ videorecog-20200320/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f6d03fa0-b289-11ed-870f-1d168b094437")]
+[assembly: Guid("38784b30-b985-11ed-87af-6f318bcdd690")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.4.0")]
-[assembly: AssemblyFileVersion("2.0.4.0")]
+[assembly: AssemblyVersion("2.0.5.0")]
+[assembly: AssemblyFileVersion("2.0.5.0")]
