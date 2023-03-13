d1d5f6fc95fde1f578144a0ffc64e3806ff99f9d
sdk-team <sdk-team@alibabacloud.com>
2023-03-13 06:50:10 +0000

    Update GenerateVideoPlaylist.

--- imm-20200930/core/Properties/AssemblyInfo.cs
+++ imm-20200930/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cd5a3cc0-be69-11ed-aee6-9701a77398ba")]
+[assembly: Guid("27239c20-c16b-11ed-a600-5d0712a7c6c6")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.1.6.0")]
-[assembly: AssemblyFileVersion("2.1.6.0")]
+[assembly: AssemblyVersion("2.1.7.0")]
+[assembly: AssemblyFileVersion("2.1.7.0")]
