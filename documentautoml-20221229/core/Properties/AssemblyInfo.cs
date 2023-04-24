df9ca78e85a4918cae36db1ee67cf8b8f69f64dd
sdk-team <sdk-team@alibabacloud.com>
2023-04-24 08:33:12 +0000

    DocumentInit.

--- documentautoml-20221229/core/Properties/AssemblyInfo.cs
+++ documentautoml-20221229/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6dff2750-e276-11ed-8c1e-1110c800f246")]
+[assembly: Guid("8cdaf240-e27a-11ed-ab8a-871ea0a9e704")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.10.0")]
-[assembly: AssemblyFileVersion("1.0.10.0")]
+[assembly: AssemblyVersion("1.0.11.0")]
+[assembly: AssemblyFileVersion("1.0.11.0")]
