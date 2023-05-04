06c9233e8fc7b27a00dd55d20cfc570bef4bff31
sdk-team <sdk-team@alibabacloud.com>
2023-05-04 06:27:46 +0000

    Support verify captcha.

--- captcha-20230305/core/Properties/AssemblyInfo.cs
+++ captcha-20230305/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("01d9f590-ea2e-11ed-8792-3d5c1dfaca93")]
+[assembly: Guid("b381fe30-ea44-11ed-940a-e398eb63250f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
