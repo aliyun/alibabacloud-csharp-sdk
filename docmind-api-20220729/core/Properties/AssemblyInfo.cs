4acfe0415d434ca1cd07d58813f5ab8ed9f17f5e
sdk-team <sdk-team@alibabacloud.com>
2023-06-02 08:18:19 +0000

    Docmind support new seven types for industry intelligence.

--- docmind-api-20220729/core/Properties/AssemblyInfo.cs
+++ docmind-api-20220729/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8db16180-a6e4-11ed-a760-4567ccc7b96d")]
+[assembly: Guid("eec17800-011d-11ee-8a58-fb67ee3daa62")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
