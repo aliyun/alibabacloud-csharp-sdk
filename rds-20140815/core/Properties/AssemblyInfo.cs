1ef81181abfd3cc8620c5da3eb2b5d5fac376d06
sdk-team <sdk-team@alibabacloud.com>
2023-07-01 15:13:51 +0000

    Fixed bugs for lack of regionId.

--- rds-20140815/core/Properties/AssemblyInfo.cs
+++ rds-20140815/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("39e32620-1759-11ee-aed5-03d516dade5e")]
+[assembly: Guid("8655ee70-1821-11ee-a9b7-319038038faf")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.1.6.0")]
-[assembly: AssemblyFileVersion("2.1.6.0")]
+[assembly: AssemblyVersion("2.1.7.0")]
+[assembly: AssemblyFileVersion("2.1.7.0")]
