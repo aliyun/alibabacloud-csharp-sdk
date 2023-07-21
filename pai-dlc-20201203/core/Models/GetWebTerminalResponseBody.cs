3b1d6476088f57352ba7024d37ba58696f6d67bf
sdk-team <sdk-team@alibabacloud.com>
2023-07-21 13:06:04 +0000

    Change the API -- GetWebTerminal.

--- pai-dlc-20201203/core/Models/GetWebTerminalResponseBody.cs
+++ pai-dlc-20201203/core/Models/GetWebTerminalResponseBody.cs
@@ -9,13 +9,13 @@ using Tea;
 namespace AlibabaCloud.SDK.Pai_dlc20201203.Models
 {
     public class GetWebTerminalResponseBody : TeaModel {
-        [NameInMap("URL")]
-        [Validation(Required=false)]
-        public string URL { get; set; }
-
-        [NameInMap("requestId")]
+        [NameInMap("RequestId")]
         [Validation(Required=false)]
         public string RequestId { get; set; }
+
+        [NameInMap("WebTerminalUrl")]
+        [Validation(Required=false)]
+        public string WebTerminalUrl { get; set; }
 
     }
 
--- pai-dlc-20201203/core/Properties/AssemblyInfo.cs
+++ pai-dlc-20201203/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ecb60f70-25ef-11ee-9944-214235403a2c")]
+[assembly: Guid("36e4e610-27c7-11ee-961f-2dbcf0e069d6")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.5.0")]
-[assembly: AssemblyFileVersion("1.2.5.0")]
+[assembly: AssemblyVersion("1.2.6.0")]
+[assembly: AssemblyFileVersion("1.2.6.0")]
