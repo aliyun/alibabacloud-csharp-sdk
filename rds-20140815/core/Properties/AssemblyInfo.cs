fde83ff871fc99207badb08be61edd4f304375b4
sdk-team <sdk-team@alibabacloud.com>
2023-07-30 15:10:50 +0000

    Fix bug ram.

--- rds-20140815/core/Properties/AssemblyInfo.cs
+++ rds-20140815/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f4fc2db0-2e21-11ee-a034-afeb52320a8b")]
+[assembly: Guid("19e6c350-2eeb-11ee-8878-c717f5b9a5eb")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.3.7.0")]
-[assembly: AssemblyFileVersion("2.3.7.0")]
+[assembly: AssemblyVersion("2.3.8.0")]
+[assembly: AssemblyFileVersion("2.3.8.0")]
