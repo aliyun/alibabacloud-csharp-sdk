4c5b775b22bb3bc87da757a35b16575912e2e305
sdk-team <sdk-team@alibabacloud.com>
2023-08-02 03:50:58 +0000

    Api update.

--- arms-20190808/core/Properties/AssemblyInfo.cs
+++ arms-20190808/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fe5b9990-3010-11ee-a395-5d4802c6261a")]
+[assembly: Guid("a12e2a80-30e7-11ee-8d31-d5b0ac249d0c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: AssemblyVersion("2.0.2.0")]
+[assembly: AssemblyFileVersion("2.0.2.0")]
