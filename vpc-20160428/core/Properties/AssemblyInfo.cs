df49870ddb1cb4d5106a98e06635b0d4726d2b29
sdk-team <sdk-team@alibabacloud.com>
2023-08-08 01:38:27 +0000

    Generated csharp 2016-04-28 for Vpc.

--- vpc-20160428/core/Properties/AssemblyInfo.cs
+++ vpc-20160428/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4cd050a0-34f0-11ee-8cca-af568e112fa2")]
+[assembly: Guid("9d3c11f0-358b-11ee-9d08-5547bde7c6c2")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
