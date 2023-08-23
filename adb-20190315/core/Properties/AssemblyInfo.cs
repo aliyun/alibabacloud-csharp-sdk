45368a9e0a8d63a32fb4cb4c25aabb428dcd09ba
sdk-team <sdk-team@alibabacloud.com>
2023-08-23 08:58:08 +0000

    Generated csharp 2019-03-15 for adb.

--- adb-20190315/core/Properties/AssemblyInfo.cs
+++ adb-20190315/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e7859f10-3c9d-11ee-b6e2-875f674de331")]
+[assembly: Guid("14affd60-4193-11ee-bde8-35801a9cc7c4")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.4.0")]
-[assembly: AssemblyFileVersion("1.0.4.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
