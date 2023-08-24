20386db3a4e67e10336674f2da81682a9aaa70c9
sdk-team <sdk-team@alibabacloud.com>
2023-08-24 17:13:05 +0000

    Generated csharp 2022-11-25 for Cloudauth.

--- cloudauth-20221125/core/Properties/AssemblyInfo.cs
+++ cloudauth-20221125/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("26e48c40-419c-11ee-a0f1-651322f9e52f")]
+[assembly: Guid("ded75a80-42a0-11ee-8ae6-832002f710f8")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.8.0")]
-[assembly: AssemblyFileVersion("1.0.8.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
