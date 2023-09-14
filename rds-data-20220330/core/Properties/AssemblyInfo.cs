f1ee27dafbb17336843f92e2befa704127e5f57a
sdk-team <sdk-team@alibabacloud.com>
2023-09-14 17:11:45 +0000

    Generated csharp 2022-03-30 for rds-data.

--- rds-data-20220330/core/Properties/AssemblyInfo.cs
+++ rds-data-20220330/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("42c7eb80-346a-11ee-9861-abfa30f52dd8")]
+[assembly: Guid("501c4440-5321-11ee-85c8-ebee1342fa8e")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
