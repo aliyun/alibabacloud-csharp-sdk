91ea8df0891704fecbde2e43df4ebd91cccaeb0f
sdk-team <sdk-team@alibabacloud.com>
2023-10-17 01:20:04 +0000

    Generated csharp 2018-01-15 for dcdn.

--- dcdn-20180115/ChangeLog.md
+++ dcdn-20180115/ChangeLog.md
@@ -1,3 +1,6 @@
+2023-10-17 Version: 1.5.2
+- Generated csharp 2018-01-15 for dcdn.
+
 2023-10-16 Version: 1.5.1
 - Generated csharp 2018-01-15 for dcdn.
 
--- dcdn-20180115/core/Properties/AssemblyInfo.cs
+++ dcdn-20180115/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ccdfbeb0-6c46-11ee-abad-d1e25fe79311")]
+[assembly: Guid("26d1c640-6c8b-11ee-8c56-058c0db3bf36")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.5.1.0")]
+[assembly: AssemblyFileVersion("1.5.1.0")]
