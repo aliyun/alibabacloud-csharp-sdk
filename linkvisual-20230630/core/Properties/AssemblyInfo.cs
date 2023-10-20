287ef6389b00b14d1b4fbd23cdd59e1826d5a7cd
sdk-team <sdk-team@alibabacloud.com>
2023-10-20 11:00:40 +0000

    Linkvisual_20230630 TeaDSL sdk.

--- linkvisual-20230630/core/Properties/AssemblyInfo.cs
+++ linkvisual-20230630/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a25b9100-6f2f-11ee-a318-a7d04407efff")]
+[assembly: Guid("d77c0e20-6f37-11ee-8bc6-3742c03fff0d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
