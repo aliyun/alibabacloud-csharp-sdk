ad6edcb712a0d1e2c1a8bc41fd99674f08331fa1
sdk-team <sdk-team@alibabacloud.com>
2023-10-24 17:11:52 +0000

    Generated csharp 2022-01-01 for quickbi-public.

--- quickbi-public-20220101/core/Properties/AssemblyInfo.cs
+++ quickbi-public-20220101/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0e55bfa0-157d-11ee-947e-9dad034662ac")]
+[assembly: Guid("f0377480-728f-11ee-9d19-9f57be10a066")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.5.0")]
-[assembly: AssemblyFileVersion("1.1.5.0")]
+[assembly: AssemblyVersion("1.1.6.0")]
+[assembly: AssemblyFileVersion("1.1.6.0")]
