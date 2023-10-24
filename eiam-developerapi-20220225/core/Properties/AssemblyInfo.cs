4d3f2e022a3e67d06e1ca64eaf336d441dca7954
sdk-team <sdk-team@alibabacloud.com>
2023-10-24 17:12:58 +0000

    Generated csharp 2022-02-25 for Eiam-developerapi.

--- eiam-developerapi-20220225/core/Properties/AssemblyInfo.cs
+++ eiam-developerapi-20220225/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b7771c00-5164-11ee-b64f-e3d2c953ab8c")]
+[assembly: Guid("042206e0-7290-11ee-a2f7-e1704a058a29")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.6.0")]
-[assembly: AssemblyFileVersion("1.0.6.0")]
+[assembly: AssemblyVersion("1.0.7.0")]
+[assembly: AssemblyFileVersion("1.0.7.0")]
