6239748dea8b0d7ceca477668eb17b65eec85017
sdk-team <sdk-team@alibabacloud.com>
2023-11-08 03:06:10 +0000

    Generated csharp 2022-05-20 for btripOpen.

--- btripopen-20220520/core/Properties/AssemblyInfo.cs
+++ btripopen-20220520/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bb581a60-7d90-11ee-b134-5931f5305568")]
+[assembly: Guid("92751960-7de3-11ee-bf1f-31429491e598")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.7.2.0")]
-[assembly: AssemblyFileVersion("1.7.2.0")]
+[assembly: AssemblyVersion("1.7.3.0")]
+[assembly: AssemblyFileVersion("1.7.3.0")]
