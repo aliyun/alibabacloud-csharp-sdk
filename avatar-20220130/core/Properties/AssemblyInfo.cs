fff3301646ee3289c0bed6f1fc4098c90d4cc0f8
sdk-team <sdk-team@alibabacloud.com>
2023-11-09 03:13:59 +0000

    Generated csharp 2022-01-30 for avatar.

--- avatar-20220130/core/Properties/AssemblyInfo.cs
+++ avatar-20220130/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6e1df430-7e59-11ee-a24a-adbdb816aa1e")]
+[assembly: Guid("e55b6c40-7ead-11ee-a449-bd1887feb498")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.3.0.0")]
-[assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyVersion("2.3.1.0")]
+[assembly: AssemblyFileVersion("2.3.1.0")]
