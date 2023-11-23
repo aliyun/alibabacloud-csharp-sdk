0c51a420e50eb1bbe6b591bf0abb97ed640a22d9
sdk-team <sdk-team@alibabacloud.com>
2023-11-23 06:01:00 +0000

    Generated csharp 2021-07-07 for ocr-api.

--- ocr-api-20210707/core/Properties/AssemblyInfo.cs
+++ ocr-api-20210707/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a93e7f50-8884-11ee-8328-934686b996ad")]
+[assembly: Guid("ae7395f0-89c4-11ee-8e86-9f90211580c1")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
