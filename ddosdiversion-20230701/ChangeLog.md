5635557c90fa5fe3f17aa882301551daa48905c8
sdk-team <sdk-team@alibabacloud.com>
2023-12-11 09:25:01 +0000

    Generated csharp 2023-07-01 for DdosDiversion.

--- ddosdiversion-20230701/ChangeLog.md
+++ ddosdiversion-20230701/ChangeLog.md
@@ -1,3 +1,6 @@
+2023-12-11 Version: 1.0.1
+- Generated csharp 2023-07-01 for DdosDiversion.
+
 2023-12-08 Version: 1.0.0
 - Generated csharp 2023-07-01 for DdosDiversion.
 
--- ddosdiversion-20230701/core/Properties/AssemblyInfo.cs
+++ ddosdiversion-20230701/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f9d56950-9596-11ee-ae6a-5f721508400d")]
+[assembly: Guid("1245ee70-9807-11ee-9e5f-5d6c1f486228")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
