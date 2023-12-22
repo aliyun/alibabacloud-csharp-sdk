dbcd9f3918d6f416e7adeb88b321be94e64f1ad0
sdk-team <sdk-team@alibabacloud.com>
2023-12-22 02:16:49 +0000

    Generated csharp 2023-03-05 for captcha.

--- captcha-20230305/ChangeLog.md
+++ captcha-20230305/ChangeLog.md
@@ -1,3 +1,6 @@
+2023-12-22 Version: 1.1.3
+- Generated csharp 2023-03-05 for captcha.
+
 2023-12-21 Version: 1.1.2
 - Generated csharp 2023-03-05 for captcha.
 
--- captcha-20230305/core/Properties/AssemblyInfo.cs
+++ captcha-20230305/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d4708ef0-a023-11ee-b60d-7bca60d23ada")]
+[assembly: Guid("134786a0-a070-11ee-b4a6-9b2ec251fd81")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.2.0")]
