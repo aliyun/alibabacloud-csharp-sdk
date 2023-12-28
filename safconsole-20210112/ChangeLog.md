63f014f9f63ad89500af1271efa8f293d467b1de
sdk-team <sdk-team@alibabacloud.com>
2023-12-28 09:26:29 +0000

    Generated csharp 2021-01-12 for safconsole.

--- safconsole-20210112/ChangeLog.md
+++ safconsole-20210112/ChangeLog.md
@@ -1,3 +1,6 @@
+2023-12-28 Version: 1.0.1
+- Generated csharp 2021-01-12 for safconsole.
+
 2023-12-27 Version: 1.0.0
 - Generated csharp 2021-01-12 for safconsole.
 
--- safconsole-20210112/core/Models/UploadSampleApiResponseBody.cs
+++ safconsole-20210112/core/Models/UploadSampleApiResponseBody.cs
@@ -9,12 +9,24 @@ using Tea;
 namespace AlibabaCloud.SDK.Safconsole20210112.Models
 {
     public class UploadSampleApiResponseBody : TeaModel {
+        [NameInMap("Code")]
+        [Validation(Required=false)]
+        public string Code { get; set; }
+
+        [NameInMap("Message")]
+        [Validation(Required=false)]
+        public string Message { get; set; }
+
         /// <summary>
         /// Id of the request
         /// </summary>
         [NameInMap("RequestId")]
         [Validation(Required=false)]
         public string RequestId { get; set; }
+
+        [NameInMap("Success")]
+        [Validation(Required=false)]
+        public string Success { get; set; }
 
     }
 
