0db8d5bc238076421bcfcb58bb6a38aa235515df
sdk-team <sdk-team@alibabacloud.com>
2024-01-04 07:19:20 +0000

    Generated csharp 2021-06-25 for devops.

--- devops-20210625/ChangeLog.md
+++ devops-20210625/ChangeLog.md
@@ -1,3 +1,6 @@
+2024-01-04 Version: 4.6.2
+- Generated csharp 2021-06-25 for devops.
+
 2024-01-03 Version: 4.6.1
 - Generated csharp 2021-06-25 for devops.
 
--- devops-20210625/core/Properties/AssemblyInfo.cs
+++ devops-20210625/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4c127120-aa1b-11ee-819b-3f29131d94b0")]
+[assembly: Guid("71dab730-aad1-11ee-96d9-71f84e568b3c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.6.0.0")]
-[assembly: AssemblyFileVersion("4.6.0.0")]
+[assembly: AssemblyVersion("4.6.1.0")]
+[assembly: AssemblyFileVersion("4.6.1.0")]
