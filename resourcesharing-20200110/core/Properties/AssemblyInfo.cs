a79a7cd928af0479ad6a7dc998110c9cd7856514
sdk-team <sdk-team@alibabacloud.com>
2024-01-08 07:28:49 +0000

    Generated csharp 2020-01-10 for ResourceSharing.

--- resourcesharing-20200110/core/Properties/AssemblyInfo.cs
+++ resourcesharing-20200110/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b4a22d90-aa5a-11ee-ba55-f375e9b797d3")]
+[assembly: Guid("7897dd90-adf7-11ee-bd4b-eb7d075adf87")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.1.1.0")]
-[assembly: AssemblyFileVersion("2.1.1.0")]
+[assembly: AssemblyVersion("2.1.2.0")]
+[assembly: AssemblyFileVersion("2.1.2.0")]
