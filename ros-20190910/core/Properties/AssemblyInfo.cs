920924b28eb6e54e8ecb032139eb53b770cdb05b
sdk-team <sdk-team@alibabacloud.com>
2024-01-11 08:41:04 +0000

    Generated csharp 2019-09-10 for ROS.

--- ros-20190910/core/Properties/AssemblyInfo.cs
+++ ros-20190910/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("55b25d20-b057-11ee-8476-7da40b16ea06")]
+[assembly: Guid("11e065f0-b05d-11ee-9d6e-3933db2a45b6")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.3.2.0")]
-[assembly: AssemblyFileVersion("2.3.2.0")]
+[assembly: AssemblyVersion("2.3.3.0")]
+[assembly: AssemblyFileVersion("2.3.3.0")]
