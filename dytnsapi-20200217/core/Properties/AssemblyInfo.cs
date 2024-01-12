8848dee9969980b59e02c39403ab6f2dd5c2d56e
sdk-team <sdk-team@alibabacloud.com>
2024-01-12 03:46:07 +0000

    Generated csharp 2020-02-17 for Dytnsapi.

--- dytnsapi-20200217/core/Properties/AssemblyInfo.cs
+++ dytnsapi-20200217/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("95a8aec0-a5f3-11ee-b8eb-3339b4626e93")]
+[assembly: Guid("030ca980-b0fd-11ee-bc8a-7774b34be2d0")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.5.0.0")]
-[assembly: AssemblyFileVersion("2.5.0.0")]
+[assembly: AssemblyVersion("2.5.1.0")]
+[assembly: AssemblyFileVersion("2.5.1.0")]
