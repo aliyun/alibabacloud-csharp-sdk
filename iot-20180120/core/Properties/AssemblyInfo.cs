c087cd4ecdc8f18b05d1a35312b842fb2d523da9
sdk-team <sdk-team@alibabacloud.com>
2024-01-15 03:08:07 +0000

    BatchPub Support MQTT5.

--- iot-20180120/core/Properties/AssemblyInfo.cs
+++ iot-20180120/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("91a13fa0-af5c-11ee-a814-9f8940fa182f")]
+[assembly: Guid("20b0a8d0-b353-11ee-849a-0d1ad6530c12")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.2.0.0")]
-[assembly: AssemblyFileVersion("4.2.0.0")]
+[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.0.0.0")]
