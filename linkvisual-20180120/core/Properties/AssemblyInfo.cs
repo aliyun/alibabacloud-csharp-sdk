fd04815d2e7281b635f37f300dea43cbd2cfea77
sdk-team <sdk-team@alibabacloud.com>
2024-01-16 09:29:02 +0000

    Generated csharp 2018-01-20 for Linkvisual.

--- linkvisual-20180120/core/Properties/AssemblyInfo.cs
+++ linkvisual-20180120/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e33febd0-b44e-11ee-9c92-cff2b4a48542")]
+[assembly: Guid("91f3aac0-b451-11ee-b0fd-21afab2c9695")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
