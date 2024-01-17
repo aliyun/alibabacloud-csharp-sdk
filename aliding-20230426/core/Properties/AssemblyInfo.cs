fdb3ba4ffeb1c2d29fc4ae1b1b6c0cb7ddd71b00
sdk-team <sdk-team@alibabacloud.com>
2024-01-17 11:53:51 +0000

    Generated csharp 2023-04-26 for aliding.

--- aliding-20230426/core/Properties/AssemblyInfo.cs
+++ aliding-20230426/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3b1688d0-b514-11ee-991e-911423b0a65e")]
+[assembly: Guid("eb27c800-b52e-11ee-b23b-e5d24ada5a91")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.4.0.0")]
-[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: AssemblyVersion("2.4.1.0")]
+[assembly: AssemblyFileVersion("2.4.1.0")]
