1dee745b9b9c9afb85379da516d1c04b64d5d9b1
sdk-team <sdk-team@alibabacloud.com>
2024-01-19 09:34:46 +0000

    Generated csharp 2021-06-25 for devops.

--- devops-20210625/core/Properties/AssemblyInfo.cs
+++ devops-20210625/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("dbf1b120-b69b-11ee-8d77-eb74b86d30e1")]
+[assembly: Guid("d3de6930-b6ad-11ee-9ed3-097b1281192a")]
 
 // Version information for an assembly consists of the following four values:
 //
