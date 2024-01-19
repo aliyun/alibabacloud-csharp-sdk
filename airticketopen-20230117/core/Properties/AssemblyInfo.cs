6fae4ab7f51f43c3937422e88bf31cebff467485
sdk-team <sdk-team@alibabacloud.com>
2024-01-19 06:15:51 +0000

    Generated csharp 2023-01-17 for airticketOpen.

--- airticketopen-20230117/core/Properties/AssemblyInfo.cs
+++ airticketopen-20230117/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0da5b780-b605-11ee-b7cc-a19f804189bb")]
+[assembly: Guid("15a9e400-b692-11ee-99e2-6d33556714d1")]
 
 // Version information for an assembly consists of the following four values:
 //
