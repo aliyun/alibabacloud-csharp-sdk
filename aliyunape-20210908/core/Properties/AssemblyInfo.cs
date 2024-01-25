b92e351ab1d0a811217923a08ebf5c2e07ca3a4f
sdk-team <sdk-team@alibabacloud.com>
2024-01-25 14:21:27 +0000

    Update Tea.

--- aliyunape-20210908/core/Properties/AssemblyInfo.cs
+++ aliyunape-20210908/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("12319ce0-8c01-11ec-99dd-6f32ada87872")]
+[assembly: Guid("a791a660-bb8c-11ee-8e38-95ebd1572593")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.0.95.0")]
+[assembly: AssemblyFileVersion("1.0.95.0")]
