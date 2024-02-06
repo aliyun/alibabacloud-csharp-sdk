5f3f090dc33baf8e55048894aff99b1e0ca810b5
sdk-team <sdk-team@alibabacloud.com>
2024-02-06 17:11:29 +0000

    Generated csharp 2021-05-20 for Linkcard.

--- linkcard-20210520/core/Models/StopSingleCardResponse.cs
+++ linkcard-20210520/core/Models/StopSingleCardResponse.cs
@@ -10,15 +10,15 @@ namespace AlibabaCloud.SDK.Linkcard20210520.Models
 {
     public class StopSingleCardResponse : TeaModel {
         [NameInMap("headers")]
-        [Validation(Required=true)]
+        [Validation(Required=false)]
         public Dictionary<string, string> Headers { get; set; }
 
         [NameInMap("statusCode")]
-        [Validation(Required=true)]
+        [Validation(Required=false)]
         public int? StatusCode { get; set; }
 
         [NameInMap("body")]
-        [Validation(Required=true)]
+        [Validation(Required=false)]
         public StopSingleCardResponseBody Body { get; set; }
 
     }
--- linkcard-20210520/core/Properties/AssemblyInfo.cs
+++ linkcard-20210520/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b68ed0b0-51d9-11ee-b13d-dbaf6075f50c")]
+[assembly: Guid("1e6eb8c0-c512-11ee-8aa2-13b46e72aa42")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.10.0")]
-[assembly: AssemblyFileVersion("1.0.10.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
