381383a6f86b7aeb00fc9cab416f3a139fda1106
sdk-team <sdk-team@alibabacloud.com>
2024-02-19 09:21:14 +0000

    Generated csharp 2022-02-20 for ecs-workbench.

--- ecs-workbench-20220220/ChangeLog.md
+++ ecs-workbench-20220220/ChangeLog.md
@@ -1,3 +1,6 @@
+2024-02-19 Version: 1.0.1
+- Generated csharp 2022-02-20 for ecs-workbench.
+
 2024-02-05 Version: 1.0.0
 - Generated csharp 2022-02-20 for ecs-workbench.
 
--- ecs-workbench-20220220/core/Properties/AssemblyInfo.cs
+++ ecs-workbench-20220220/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("14774c60-c404-11ee-b77d-f1e43f930799")]
+[assembly: Guid("1a59ba00-cf08-11ee-867e-6b3bce2c58d7")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
