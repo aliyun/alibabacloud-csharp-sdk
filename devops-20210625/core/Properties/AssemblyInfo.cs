cf50f8c48c2539bb34f13510c486d50962dd1ab1
sdk-team <sdk-team@alibabacloud.com>
2024-02-27 09:37:14 +0000

    Support API ListUserDrawRecordByPk.

--- devops-20210625/core/Properties/AssemblyInfo.cs
+++ devops-20210625/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("324ee260-d54e-11ee-82bd-65c9056775e0")]
+[assembly: Guid("938ff0f0-d553-11ee-9dad-37ce29c1b0a7")]
 
 // Version information for an assembly consists of the following four values:
 //
