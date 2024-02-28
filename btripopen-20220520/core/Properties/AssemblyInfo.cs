d8c189e649cdd0e13901b09b4a6a64abe7dde6f8
sdk-team <sdk-team@alibabacloud.com>
2024-02-28 10:05:07 +0000

    Support API CooperatorFlightBillSettlementQuery.

--- btripopen-20220520/core/Properties/AssemblyInfo.cs
+++ btripopen-20220520/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c47111b0-d592-11ee-92ff-0f9cb04cdc0e")]
+[assembly: Guid("ba95c8a0-d620-11ee-a355-791ed5d1aaf8")]
 
 // Version information for an assembly consists of the following four values:
 //
