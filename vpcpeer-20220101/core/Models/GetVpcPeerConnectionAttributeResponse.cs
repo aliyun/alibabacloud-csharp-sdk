8d3b1e1b44034b1bfacd2f73dbd628e385994673
sdk-team <sdk-team@alibabacloud.com>
2024-03-12 17:19:46 +0000

    Generated csharp 2022-01-01 for VpcPeer.

--- vpcpeer-20220101/core/Models/GetVpcPeerConnectionAttributeResponse.cs
+++ vpcpeer-20220101/core/Models/GetVpcPeerConnectionAttributeResponse.cs
@@ -10,15 +10,15 @@ namespace AlibabaCloud.SDK.VpcPeer20220101.Models
 {
     public class GetVpcPeerConnectionAttributeResponse : TeaModel {
         [NameInMap("headers")]
-        [Validation(Required=true)]
+        [Validation(Required=false)]
         public Dictionary<string, string> Headers { get; set; }
 
         [NameInMap("statusCode")]
-        [Validation(Required=true)]
+        [Validation(Required=false)]
         public int? StatusCode { get; set; }
 
         [NameInMap("body")]
-        [Validation(Required=true)]
+        [Validation(Required=false)]
         public GetVpcPeerConnectionAttributeResponseBody Body { get; set; }
 
     }
--- vpcpeer-20220101/core/Properties/AssemblyInfo.cs
+++ vpcpeer-20220101/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1fe20d70-ba12-11ee-bffc-8fc29cf4edd4")]
+[assembly: Guid("72f865c0-e093-11ee-85f6-8fbd7d1af628")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
