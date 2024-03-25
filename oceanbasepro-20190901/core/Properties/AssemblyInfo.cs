472023a1c537000cca055d22059fcefe598296f3
sdk-team <sdk-team@alibabacloud.com>
2024-03-25 12:42:47 +0000

    Generated csharp 2019-09-01 for OceanBasePro.

--- oceanbasepro-20190901/core/Properties/AssemblyInfo.cs
+++ oceanbasepro-20190901/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ab4153b0-e688-11ee-bb76-5b834e629b96")]
+[assembly: Guid("0a376510-eaa5-11ee-b7dd-a3a572f90753")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.4.0.0")]
-[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
