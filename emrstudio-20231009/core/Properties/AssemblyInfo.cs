0037c6e57d3a2a3a6616b6bdba99b66e1c3a5982
sdk-team <sdk-team@alibabacloud.com>
2024-03-27 13:27:12 +0000

    Update API DescribeWorkflow: update param projectId.

--- emrstudio-20231009/core/Properties/AssemblyInfo.cs
+++ emrstudio-20231009/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1e341960-ec25-11ee-9d23-7bb0f50ded58")]
+[assembly: Guid("979293f0-ec3d-11ee-b20f-1383fd566efe")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
