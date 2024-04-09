493855e0884f239b94d8b4b269e0cdfec957b950
sdk-team <sdk-team@alibabacloud.com>
2024-04-09 07:26:15 +0000

    Generated csharp 2023-12-30 for SysOM.

--- sysom-20231230/core/Properties/AssemblyInfo.cs
+++ sysom-20231230/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d128b510-be6e-11ee-9ea2-f1e94c4a7aa1")]
+[assembly: Guid("5847f7d0-f642-11ee-8bbf-7f6f9335d5e8")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
