a81a8e22737559fb8e79c2e6e0502eacb47a390f
sdk-team <sdk-team@alibabacloud.com>
2024-04-09 13:28:12 +0000

    Delete API CreateOrderForCreateDBNodes.

--- rds-20140815/core/Properties/AssemblyInfo.cs
+++ rds-20140815/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8a845050-f614-11ee-9f85-7bdbba8392f2")]
+[assembly: Guid("d624a8b0-f674-11ee-912c-31ef8b755fa2")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyVersion("4.0.1.0")]
+[assembly: AssemblyFileVersion("4.0.1.0")]
