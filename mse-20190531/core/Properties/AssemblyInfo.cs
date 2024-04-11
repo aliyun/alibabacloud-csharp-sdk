c2cd2b6544eb4bae1bfb7cc9feea8f3e27acd44c
sdk-team <sdk-team@alibabacloud.com>
2024-04-11 11:12:11 +0000

    Generated csharp 2019-05-31 for mse.

--- mse-20190531/core/Properties/AssemblyInfo.cs
+++ mse-20190531/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fc707860-f7b3-11ee-a0cb-7b071bc4e125")]
+[assembly: Guid("2f545180-f7f4-11ee-b19f-f735ca7ecd94")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("6.3.3.0")]
-[assembly: AssemblyFileVersion("6.3.3.0")]
+[assembly: AssemblyVersion("6.4.0.0")]
+[assembly: AssemblyFileVersion("6.4.0.0")]
