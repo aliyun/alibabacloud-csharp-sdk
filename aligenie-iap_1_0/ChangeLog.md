2ce010acad16de424f5df37f8a706f4bb1a72596
sdk-team <sdk-team@alibabacloud.com>
2024-04-17 17:16:12 +0000

    Generated csharp iap_1.0 for AliGenie.

--- aligenie-iap_1_0/ChangeLog.md
+++ aligenie-iap_1_0/ChangeLog.md
@@ -1,3 +1,6 @@
+2024-04-17 Version: 1.0.1
+- Generated csharp iap_1.0 for AliGenie.
+
 2024-04-16 Version: 1.0.0
 - Support API CallBackThirdRightSendPlan.
 - Support API CheckThirdRightSendPlan.
--- aligenie-iap_1_0/core/Properties/AssemblyInfo.cs
+++ aligenie-iap_1_0/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c7f0a880-fc13-11ee-b057-3355e869d611")]
+[assembly: Guid("efba9050-fcdc-11ee-aab4-c541817e3cc5")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
