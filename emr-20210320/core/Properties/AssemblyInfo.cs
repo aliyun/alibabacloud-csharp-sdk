a99c01184a164e01b0c9e4d0c3f0c996ca98e377
sdk-team <sdk-team@alibabacloud.com>
2024-04-19 05:14:10 +0000

    Generated csharp 2021-03-20 for Emr.

--- emr-20210320/core/Properties/AssemblyInfo.cs
+++ emr-20210320/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b608a2f0-fd7e-11ee-95fa-e7c7567de1c0")]
+[assembly: Guid("8413f5a0-fe0b-11ee-b629-5533fa5ca613")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.2.0")]
-[assembly: AssemblyFileVersion("1.4.2.0")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
