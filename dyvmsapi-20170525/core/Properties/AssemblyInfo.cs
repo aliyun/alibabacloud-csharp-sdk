a54f8503b2c3465b24948a60c5d4fe0fb085f128
sdk-team <sdk-team@alibabacloud.com>
2024-04-26 17:10:23 +0000

    Generated csharp 2017-05-25 for Dyvmsapi.

--- dyvmsapi-20170525/core/Properties/AssemblyInfo.cs
+++ dyvmsapi-20170525/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6859a240-0326-11ef-97b7-bf1707efa85c")]
+[assembly: Guid("6b80b980-03ef-11ef-b4d6-133a4d08106e")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("3.2.0.0")]
+[assembly: AssemblyFileVersion("3.2.0.0")]
