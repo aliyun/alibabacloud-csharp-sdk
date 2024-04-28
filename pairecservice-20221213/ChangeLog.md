f57d37c5a190e6a13ae44aaa6d700bd925cb24f3
sdk-team <sdk-team@alibabacloud.com>
2024-04-28 01:35:05 +0000

    Generated csharp 2022-12-13 for PaiRecService.

--- pairecservice-20221213/ChangeLog.md
+++ pairecservice-20221213/ChangeLog.md
@@ -1,3 +1,6 @@
+2024-04-28 Version: 1.1.3
+- Generated csharp 2022-12-13 for PaiRecService.
+
 2024-03-22 Version: 1.1.2
 - Generated csharp 2022-12-13 for PaiRecService.
 
--- pairecservice-20221213/core/Properties/AssemblyInfo.cs
+++ pairecservice-20221213/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4670da80-e7fc-11ee-b039-af3e8ffe8989")]
+[assembly: Guid("74c0f8f0-04ff-11ef-bb2b-33cd854f7b0d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.2.0")]
