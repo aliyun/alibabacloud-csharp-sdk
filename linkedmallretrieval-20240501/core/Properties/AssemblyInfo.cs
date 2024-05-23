101b994a4269db9e51963c062ba082fc6418721f
sdk-team <sdk-team@alibabacloud.com>
2024-05-23 10:22:51 +0000

    Generated csharp 2024-05-01 for LinkedmallRetrieval.

--- linkedmallretrieval-20240501/core/Properties/AssemblyInfo.cs
+++ linkedmallretrieval-20240501/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("85f7f810-1429-11ef-80c0-413ed34b426e")]
+[assembly: Guid("4fa65a80-18ee-11ef-825e-ef237465bde0")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
