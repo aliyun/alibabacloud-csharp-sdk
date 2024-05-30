820538fe085ac4b0537ecfa57c230a347ea36795
sdk-team <sdk-team@alibabacloud.com>
2024-05-30 12:44:07 +0000

    Generated csharp 2014-05-15 for Slb.

--- slb-20140515/ChangeLog.md
+++ slb-20140515/ChangeLog.md
@@ -1,3 +1,6 @@
+2024-05-30 Version: 2.0.8
+- Generated csharp 2014-05-15 for Slb.
+
 2024-05-28 Version: 2.0.7
 - Update API DescribeLoadBalancerHTTPListenerAttribute: update response param.
 - Update API DescribeLoadBalancerHTTPSListenerAttribute: update response param.
--- slb-20140515/core/Properties/AssemblyInfo.cs
+++ slb-20140515/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("451f1b80-1d15-11ef-acc5-6bc0504d1a4c")]
+[assembly: Guid("37ebd980-1e82-11ef-8d11-eb608b67d11a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.6.0")]
-[assembly: AssemblyFileVersion("2.0.6.0")]
+[assembly: AssemblyVersion("2.0.7.0")]
+[assembly: AssemblyFileVersion("2.0.7.0")]
