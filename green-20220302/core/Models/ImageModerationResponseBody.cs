20bd9bba4414777f269ea153417a5be93643047d
sdk-team <sdk-team@alibabacloud.com>
2024-05-30 17:14:27 +0000

    Update API ImageModeration: update response param.

--- green-20220302/core/Models/ImageModerationResponseBody.cs
+++ green-20220302/core/Models/ImageModerationResponseBody.cs
@@ -25,6 +25,70 @@ namespace AlibabaCloud.SDK.Green20220302.Models
             [Validation(Required=false)]
             public ImageModerationResponseBodyDataExt Ext { get; set; }
             public class ImageModerationResponseBodyDataExt : TeaModel {
+                [NameInMap("CustomImage")]
+                [Validation(Required=false)]
+                public List<ImageModerationResponseBodyDataExtCustomImage> CustomImage { get; set; }
+                public class ImageModerationResponseBodyDataExtCustomImage : TeaModel {
+                    [NameInMap("ImageId")]
+                    [Validation(Required=false)]
+                    public string ImageId { get; set; }
+
+                    [NameInMap("LibId")]
+                    [Validation(Required=false)]
+                    public string LibId { get; set; }
+
+                    [NameInMap("LibName")]
+                    [Validation(Required=false)]
+                    public string LibName { get; set; }
+
+                }
+
+                [NameInMap("LogoData")]
+                [Validation(Required=false)]
+                public List<ImageModerationResponseBodyDataExtLogoData> LogoData { get; set; }
+                public class ImageModerationResponseBodyDataExtLogoData : TeaModel {
+                    [NameInMap("Location")]
+                    [Validation(Required=false)]
+                    public ImageModerationResponseBodyDataExtLogoDataLocation Location { get; set; }
+                    public class ImageModerationResponseBodyDataExtLogoDataLocation : TeaModel {
+                        [NameInMap("H")]
+                        [Validation(Required=false)]
+                        public int? H { get; set; }
+
+                        [NameInMap("W")]
+                        [Validation(Required=false)]
+                        public int? W { get; set; }
+
+                        [NameInMap("X")]
+                        [Validation(Required=false)]
+                        public int? X { get; set; }
+
+                        [NameInMap("Y")]
+                        [Validation(Required=false)]
+                        public int? Y { get; set; }
+
+                    }
+
+                    [NameInMap("Logo")]
+                    [Validation(Required=false)]
+                    public List<ImageModerationResponseBodyDataExtLogoDataLogo> Logo { get; set; }
+                    public class ImageModerationResponseBodyDataExtLogoDataLogo : TeaModel {
+                        [NameInMap("Confidence")]
+                        [Validation(Required=false)]
+                        public float? Confidence { get; set; }
+
+                        [NameInMap("Label")]
+                        [Validation(Required=false)]
+                        public string Label { get; set; }
+
+                        [NameInMap("Name")]
+                        [Validation(Required=false)]
+                        public string Name { get; set; }
+
+                    }
+
+                }
+
                 [NameInMap("OcrResult")]
                 [Validation(Required=false)]
                 public List<ImageModerationResponseBodyDataExtOcrResult> OcrResult { get; set; }
@@ -57,6 +121,20 @@ namespace AlibabaCloud.SDK.Green20220302.Models
 
                 }
 
+                [NameInMap("PublicFigure")]
+                [Validation(Required=false)]
+                public List<ImageModerationResponseBodyDataExtPublicFigure> PublicFigure { get; set; }
+                public class ImageModerationResponseBodyDataExtPublicFigure : TeaModel {
+                    [NameInMap("FigureId")]
+                    [Validation(Required=false)]
+                    public string FigureId { get; set; }
+
+                    [NameInMap("FigureName")]
+                    [Validation(Required=false)]
+                    public string FigureName { get; set; }
+
+                }
+
                 [NameInMap("Recognition")]
                 [Validation(Required=false)]
                 public List<ImageModerationResponseBodyDataExtRecognition> Recognition { get; set; }
@@ -68,6 +146,66 @@ namespace AlibabaCloud.SDK.Green20220302.Models
                     [NameInMap("Confidence")]
                     [Validation(Required=false)]
                     public float? Confidence { get; set; }
+
+                }
+
+                [NameInMap("TextInImage")]
+                [Validation(Required=false)]
+                public ImageModerationResponseBodyDataExtTextInImage TextInImage { get; set; }
+                public class ImageModerationResponseBodyDataExtTextInImage : TeaModel {
+                    [NameInMap("CustomText")]
+                    [Validation(Required=false)]
+                    public List<ImageModerationResponseBodyDataExtTextInImageCustomText> CustomText { get; set; }
+                    public class ImageModerationResponseBodyDataExtTextInImageCustomText : TeaModel {
+                        [NameInMap("KeyWords")]
+                        [Validation(Required=false)]
+                        public string KeyWords { get; set; }
+
+                        [NameInMap("LibId")]
+                        [Validation(Required=false)]
+                        public string LibId { get; set; }
+
+                        [NameInMap("LibName")]
+                        [Validation(Required=false)]
+                        public string LibName { get; set; }
+
+                    }
+
+                    [NameInMap("OcrResult")]
+                    [Validation(Required=false)]
+                    public List<ImageModerationResponseBodyDataExtTextInImageOcrResult> OcrResult { get; set; }
+                    public class ImageModerationResponseBodyDataExtTextInImageOcrResult : TeaModel {
+                        [NameInMap("Location")]
+                        [Validation(Required=false)]
+                        public ImageModerationResponseBodyDataExtTextInImageOcrResultLocation Location { get; set; }
+                        public class ImageModerationResponseBodyDataExtTextInImageOcrResultLocation : TeaModel {
+                            [NameInMap("H")]
+                            [Validation(Required=false)]
+                            public int? H { get; set; }
+
+                            [NameInMap("W")]
+                            [Validation(Required=false)]
+                            public int? W { get; set; }
+
+                            [NameInMap("X")]
+                            [Validation(Required=false)]
+                            public int? X { get; set; }
+
+                            [NameInMap("Y")]
+                            [Validation(Required=false)]
+                            public int? Y { get; set; }
+
+                        }
+
+                        [NameInMap("Text")]
+                        [Validation(Required=false)]
+                        public string Text { get; set; }
+
+                    }
+
+                    [NameInMap("RiskWord")]
+                    [Validation(Required=false)]
+                    public List<string> RiskWord { get; set; }
 
                 }
 
--- green-20220302/core/Properties/AssemblyInfo.cs
+++ green-20220302/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("10c3b0e0-1c01-11ef-95b4-1b4672b71393")]
+[assembly: Guid("25992560-1ea7-11ef-b8d1-b3acb0c34ca9")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.2.5.0")]
-[assembly: AssemblyFileVersion("2.2.5.0")]
+[assembly: AssemblyVersion("2.2.6.0")]
+[assembly: AssemblyFileVersion("2.2.6.0")]
