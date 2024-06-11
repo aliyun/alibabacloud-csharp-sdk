cd1dc15e5e46511b5d09d31d56d1bd6cd0214370
sdk-team <sdk-team@alibabacloud.com>
2024-06-11 02:28:36 +0000

    Generated csharp 2023-01-20 for csas.

--- csas-20230120/core/Properties/AssemblyInfo.cs
+++ csas-20230120/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("696bacf0-24f0-11ef-badb-cd18e133c728")]
+[assembly: Guid("270153e0-279a-11ef-af2e-f77cd76caea2")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
