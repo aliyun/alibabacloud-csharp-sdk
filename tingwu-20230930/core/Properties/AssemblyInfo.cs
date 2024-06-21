46c674f87412ab330149607c7a2aa76c23da72d9
sdk-team <sdk-team@alibabacloud.com>
2024-06-21 08:24:45 +0000

    Generated csharp 2023-09-30 for tingwu.

--- tingwu-20230930/core/Properties/AssemblyInfo.cs
+++ tingwu-20230930/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("758da800-2fa2-11ef-b3c0-c7cad3102354")]
+[assembly: Guid("96f95020-2fa7-11ef-9d63-c15340cbc9ca")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.12.0")]
-[assembly: AssemblyFileVersion("2.0.12.0")]
+[assembly: AssemblyVersion("2.0.13.0")]
+[assembly: AssemblyFileVersion("2.0.13.0")]
