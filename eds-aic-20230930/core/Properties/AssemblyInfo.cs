bf34ece246b21c827cac385478b09a3c33d20c6c
sdk-team <sdk-team@alibabacloud.com>
2024-06-27 09:18:05 +0000

    Update API CreateApp: add param BizRegionId.

--- eds-aic-20230930/core/Properties/AssemblyInfo.cs
+++ eds-aic-20230930/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c3ba56a0-33de-11ef-9ac2-2b33710f5e02")]
+[assembly: Guid("fc7a00d0-3465-11ef-b499-73c0bf1ee698")]
 
 // Version information for an assembly consists of the following four values:
 //
