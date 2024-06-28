92b658acb54824cdbd3a95a8c2011d15be6c1e2c
sdk-team <sdk-team@alibabacloud.com>
2024-06-28 06:20:35 +0000

    Generated csharp 2017-08-01 for polardb.

--- polardb-20170801/core/Properties/AssemblyInfo.cs
+++ polardb-20170801/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("89ad9900-3315-11ef-8226-d976400ad1cf")]
+[assembly: Guid("6b072e00-3516-11ef-a115-439e2438c444")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("5.1.3.0")]
-[assembly: AssemblyFileVersion("5.1.3.0")]
+[assembly: AssemblyVersion("5.1.4.0")]
+[assembly: AssemblyFileVersion("5.1.4.0")]
