12378cf8a5379d2e40afde1998616218d8b142ab
sdk-team <sdk-team@alibabacloud.com>
2024-07-22 03:05:11 +0000

    Generated csharp 2024-03-13 for IntelligentCreation.

--- intelligentcreation-20240313/core/Properties/AssemblyInfo.cs
+++ intelligentcreation-20240313/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9f47e070-459b-11ef-bb11-9da8b15b7f5a")]
+[assembly: Guid("0110be70-47d7-11ef-a293-adf56d66c81f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.1.1.0")]
+[assembly: AssemblyFileVersion("2.1.1.0")]
