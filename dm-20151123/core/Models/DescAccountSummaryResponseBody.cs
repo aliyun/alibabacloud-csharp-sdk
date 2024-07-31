43c0c6d4e511920c1dc288015ef3ef70c5a9dccd
sdk-team <sdk-team@alibabacloud.com>
2024-07-31 17:25:26 +0000

    Update API DescAccountSummary: update response param.

--- dm-20151123/core/Models/DescAccountSummaryResponseBody.cs
+++ dm-20151123/core/Models/DescAccountSummaryResponseBody.cs
@@ -13,6 +13,10 @@ namespace AlibabaCloud.SDK.Dm20151123.Models
         [Validation(Required=false)]
         public int? DailyQuota { get; set; }
 
+        [NameInMap("DailyRemainFreeQuota")]
+        [Validation(Required=false)]
+        public int? DailyRemainFreeQuota { get; set; }
+
         [NameInMap("DayuStatus")]
         [Validation(Required=false)]
         public int? DayuStatus { get; set; }
--- dm-20151123/core/Properties/AssemblyInfo.cs
+++ dm-20151123/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2c1d2e40-4396-11ef-8291-e15ebe07cdf9")]
+[assembly: Guid("e0cc7d80-4f5f-11ef-9a81-3b2beebd6386")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.2.1.0")]
