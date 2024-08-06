8d3b9ce77889b2cb41179c6a7b1429e0d8717c12
sdk-team <sdk-team@alibabacloud.com>
2024-08-06 03:17:37 +0000

    Generated csharp 2022-02-22 for Ess.

--- ess-20220222/core/Properties/AssemblyInfo.cs
+++ ess-20220222/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2cff70e0-534d-11ef-a9c1-eb056b79c89e")]
+[assembly: Guid("4d239680-53a2-11ef-8ce7-bd4cffc3adf9")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.7.2.0")]
-[assembly: AssemblyFileVersion("1.7.2.0")]
+[assembly: AssemblyVersion("1.7.3.0")]
+[assembly: AssemblyFileVersion("1.7.3.0")]
