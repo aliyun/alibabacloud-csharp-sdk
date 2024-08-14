66ad48fb66840d05bf217b45914ce7da7ef504bc
sdk-team <sdk-team@alibabacloud.com>
2024-08-14 17:15:00 +0000

    Update API ListQueueUpStreamBindings: update param MaxResults.

--- amqp-open-20191212/core/Properties/AssemblyInfo.cs
+++ amqp-open-20191212/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8abd08c0-4e96-11ef-849d-47ca78ffff6e")]
+[assembly: Guid("cfa75e20-5a5f-11ef-b5f4-2de6c802e5e2")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
