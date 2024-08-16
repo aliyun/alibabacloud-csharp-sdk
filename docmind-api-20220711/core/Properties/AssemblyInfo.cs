c04a81409d29abe7a6190a5b358e154d5b6ca347
sdk-team <sdk-team@alibabacloud.com>
2024-08-16 11:55:54 +0000

    Generated csharp 2022-07-11 for docmind-api.

--- docmind-api-20220711/core/Properties/AssemblyInfo.cs
+++ docmind-api-20220711/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3d052640-5bc4-11ef-ae48-f9a7dbb94705")]
+[assembly: Guid("56e44f30-5bc6-11ef-a09f-318e57e8b0ee")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.1.0")]
-[assembly: AssemblyFileVersion("1.3.1.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
