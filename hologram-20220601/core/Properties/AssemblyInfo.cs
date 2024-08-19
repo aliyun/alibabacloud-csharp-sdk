995b7c558d364ac2c248527bc942ac0fdc43c76d
sdk-team <sdk-team@alibabacloud.com>
2024-08-19 02:32:58 +0000

    Generated csharp 2022-06-01 for Hologram.

--- hologram-20220601/core/Properties/AssemblyInfo.cs
+++ hologram-20220601/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("09c89bd0-5bf2-11ef-9716-7d5b01d52d76")]
+[assembly: Guid("3954b4f0-5dd3-11ef-8d32-b51e9f2f698d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.1.0")]
-[assembly: AssemblyFileVersion("1.3.1.0")]
+[assembly: AssemblyVersion("1.3.2.0")]
+[assembly: AssemblyFileVersion("1.3.2.0")]
