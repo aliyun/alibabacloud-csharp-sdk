38764bab459981d996657c7a25c9ca77a0312418
sdk-team <sdk-team@alibabacloud.com>
2024-08-30 07:20:21 +0000

    Delete API CreateWHClientCertificate.

--- cas-20200407/core/Properties/AssemblyInfo.cs
+++ cas-20200407/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2e93aed0-5f69-11ef-9483-c75784e186ee")]
+[assembly: Guid("3057ddc0-66a0-11ef-ae80-ffe2a07d9c2a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.8.0")]
-[assembly: AssemblyFileVersion("1.0.8.0")]
+[assembly: AssemblyVersion("1.3.3.0")]
+[assembly: AssemblyFileVersion("1.3.3.0")]
