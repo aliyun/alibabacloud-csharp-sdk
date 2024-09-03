534ec1803c08405373958a0d0d1ab5ddb816a635
sdk-team <sdk-team@alibabacloud.com>
2024-09-03 16:27:13 +0000

    Generated csharp 2024-08-15 for TrafficFxOpen.

--- trafficfxopen-20240815/core/Properties/AssemblyInfo.cs
+++ trafficfxopen-20240815/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ce7da850-652d-11ef-b0b3-c3cc8e4fd4a2")]
+[assembly: Guid("335c4cd0-6a11-11ef-82bd-7b3e321d00ee")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
