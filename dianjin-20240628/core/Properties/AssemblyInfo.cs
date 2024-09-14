678d2351adc7152b2e8b4abdd114baa142d55378
sdk-team <sdk-team@alibabacloud.com>
2024-09-14 10:16:46 +0000

    Generated csharp 2024-06-28 for DianJin.

--- dianjin-20240628/core/Properties/AssemblyInfo.cs
+++ dianjin-20240628/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("02284380-6363-11ef-98f4-c90aab6f0868")]
+[assembly: Guid("45a4d500-7282-11ef-9daf-7f23c4a04b55")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
