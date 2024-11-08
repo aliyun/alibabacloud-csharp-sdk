e370c249644ddf9489c7f58ad27e802926cbb13e
sdk-team <sdk-team@alibabacloud.com>
2024-11-08 06:25:17 +0000

    Generated csharp 2022-03-02 for Green.

--- green-20220302/core/Properties/AssemblyInfo.cs
+++ green-20220302/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("06a584c0-9d2b-11ef-9673-e5630db19b2d")]
+[assembly: Guid("27467cb0-9d9a-11ef-bccb-4fabe8a04c26")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.2.13.0")]
-[assembly: AssemblyFileVersion("2.2.13.0")]
+[assembly: AssemblyVersion("2.2.15.0")]
+[assembly: AssemblyFileVersion("2.2.15.0")]
