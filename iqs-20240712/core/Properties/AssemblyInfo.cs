8c7d53e5bc71db716eca1dd8f356901bae4c8d9b
sdk-team <sdk-team@alibabacloud.com>
2024-11-22 08:25:22 +0000

    Delete API BicyclingDirection.

--- iqs-20240712/core/Properties/AssemblyInfo.cs
+++ iqs-20240712/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f590eec0-a259-11ef-bdb6-759f5d32a41e")]
+[assembly: Guid("fad1b9d0-a8aa-11ef-9ce1-279ec4ee46c0")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
