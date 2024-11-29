9f75c05eafcd9995edf33a56a86d0ab944ac0768
sdk-team <sdk-team@alibabacloud.com>
2024-11-29 07:04:14 +0000

    Generated csharp 2021-09-31 for BPStudio.

--- bpstudio-20210931/core/Properties/AssemblyInfo.cs
+++ bpstudio-20210931/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fce79f40-9d2a-11ef-8248-2ba156279074")]
+[assembly: Guid("c2c11e30-ae1f-11ef-851a-d5565cfde07a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.0.1.0")]
-[assembly: AssemblyFileVersion("4.0.1.0")]
+[assembly: AssemblyVersion("4.0.2.0")]
+[assembly: AssemblyFileVersion("4.0.2.0")]
