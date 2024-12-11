0e2c3ea5a1db884f3f26e8d50189b2b8d5310676
sdk-team <sdk-team@alibabacloud.com>
2024-12-11 02:03:00 +0000

    Generated csharp 2020-04-07 for cas.

--- cas-20200407/core/Properties/AssemblyInfo.cs
+++ cas-20200407/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("34112920-b719-11ef-bb8b-2df7c054c356")]
+[assembly: Guid("d0d39ff0-b763-11ef-b92b-23ab483dcb62")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.2.0")]
-[assembly: AssemblyFileVersion("2.0.2.0")]
+[assembly: AssemblyVersion("2.0.3.0")]
+[assembly: AssemblyFileVersion("2.0.3.0")]
