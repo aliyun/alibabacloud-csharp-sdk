728eec795098cb8a761a819e798f3998f7a95515
sdk-team <sdk-team@alibabacloud.com>
2024-12-12 17:15:23 +0000

    Generated csharp 2019-06-01 for oos.

--- oos-20190601/core/Properties/AssemblyInfo.cs
+++ oos-20190601/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("63ca7800-b7e2-11ef-9077-71a574be0d2b")]
+[assembly: Guid("8180e040-b8ab-11ef-b09b-593540c1c1ad")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.3.0.0")]
-[assembly: AssemblyFileVersion("3.3.0.0")]
+[assembly: AssemblyVersion("3.4.0.0")]
+[assembly: AssemblyFileVersion("3.4.0.0")]
