93cda43509f76e1a86aac160f2b1901d3e864515
sdk-team <sdk-team@alibabacloud.com>
2024-12-18 06:55:29 +0000

    Generated csharp 2023-05-22 for selectdb.

--- selectdb-20230522/core/Properties/AssemblyInfo.cs
+++ selectdb-20230522/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a58a3bf0-bd09-11ef-b633-3bd9c92348a1")]
+[assembly: Guid("ff4bf540-bd0c-11ef-b695-f9a6c7524590")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.0.1.0")]
-[assembly: AssemblyFileVersion("4.0.1.0")]
+[assembly: AssemblyVersion("4.1.0.0")]
+[assembly: AssemblyFileVersion("4.1.0.0")]
