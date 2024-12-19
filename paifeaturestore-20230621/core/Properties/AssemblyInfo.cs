3281fc538391cd0c90e5ac0ed5d24d81b9406c0f
sdk-team <sdk-team@alibabacloud.com>
2024-12-19 06:37:49 +0000

    Generated csharp 2023-06-21 for PaiFeatureStore.

--- paifeaturestore-20230621/core/Properties/AssemblyInfo.cs
+++ paifeaturestore-20230621/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("aa1852f0-bd32-11ef-a339-5dbbbf3383f5")]
+[assembly: Guid("8b7a4550-bdd3-11ef-9424-e14f7c86e60c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.1.0")]
