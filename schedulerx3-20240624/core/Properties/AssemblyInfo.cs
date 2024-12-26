c63b938d9d2c288b646f890bd777ee9f06f32f13
sdk-team <sdk-team@alibabacloud.com>
2024-12-26 11:14:39 +0000

    Generated csharp 2024-06-24 for SchedulerX3.

--- schedulerx3-20240624/core/Properties/AssemblyInfo.cs
+++ schedulerx3-20240624/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("23497b70-c36b-11ef-973e-41be58bc0a8f")]
+[assembly: Guid("6c2ed650-c37a-11ef-aca9-1157e76ff3b3")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
