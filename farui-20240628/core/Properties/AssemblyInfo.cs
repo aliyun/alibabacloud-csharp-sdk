343abf33dc99f73d359178762c641d52a802d079
sdk-team <sdk-team@alibabacloud.com>
2024-12-26 14:27:57 +0000

    Update API CreateTextFile: add param regionId.

--- farui-20240628/core/Properties/AssemblyInfo.cs
+++ farui-20240628/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f4197950-90e7-11ef-8dbc-8586b6b7b529")]
+[assembly: Guid("88f3d7c0-c395-11ef-8627-ddd97afe595b")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
