fc883c2de9241794697b74d221f2f862b1bd8917
sdk-team <sdk-team@alibabacloud.com>
2025-01-09 06:06:54 +0000

    Generated csharp 2020-02-02 for polardbx.

--- polardbx-20200202/core/Properties/AssemblyInfo.cs
+++ polardbx-20200202/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("59649910-cde3-11ef-9fc9-015dbdef33f1")]
+[assembly: Guid("d71153f0-ce4f-11ef-bec0-d760d5248330")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
