7e02d2352dd41141b7d1b56acf030e3545e4c4d0
sdk-team <sdk-team@alibabacloud.com>
2025-01-10 03:15:22 +0000

    Generated csharp 2022-03-01 for pds.

--- pds-20220301/ChangeLog.md
+++ pds-20220301/ChangeLog.md
@@ -1,3 +1,6 @@
+2025-01-10 Version: 4.0.3
+- Generated csharp 2022-03-01 for pds.
+
 2025-01-10 Version: 4.0.2
 - Generated csharp 2022-03-01 for pds.
 
--- pds-20220301/core/Properties/AssemblyInfo.cs
+++ pds-20220301/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7a86d840-cefd-11ef-aff4-d1ea31ee146f")]
+[assembly: Guid("e6503320-cf00-11ef-ab20-d9efd600db4b")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.0.1.0")]
-[assembly: AssemblyFileVersion("4.0.1.0")]
+[assembly: AssemblyVersion("4.0.2.0")]
+[assembly: AssemblyFileVersion("4.0.2.0")]
