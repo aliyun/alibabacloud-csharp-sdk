49f5131055a808ce54bab68aadb5149396441204
sdk-team <sdk-team@alibabacloud.com>
2025-02-28 09:53:33 +0000

    Support API CreateMessage.

--- aliding-20230426/core/Properties/AssemblyInfo.cs
+++ aliding-20230426/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("37b658b0-f5a9-11ef-8843-c39998ecad78")]
+[assembly: Guid("81155af0-f5b9-11ef-ac96-0de002b6eff9")]
 
 // Version information for an assembly consists of the following four values:
 //
