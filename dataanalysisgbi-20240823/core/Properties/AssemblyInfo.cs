1be26b2e3a72cea4dc08a6486ffbde169a05c72b
sdk-team <sdk-team@alibabacloud.com>
2025-03-14 06:12:12 +0000

    Generated csharp 2024-08-23 for DataAnalysisGBI.

--- dataanalysisgbi-20240823/core/Properties/AssemblyInfo.cs
+++ dataanalysisgbi-20240823/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("83b65d30-0091-11f0-8a4b-a119cfd0d7a3")]
+[assembly: Guid("1a5081e0-009b-11f0-bc6e-434d99106b1d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.3.0")]
-[assembly: AssemblyFileVersion("1.3.3.0")]
+[assembly: AssemblyVersion("1.3.4.0")]
+[assembly: AssemblyFileVersion("1.3.4.0")]
