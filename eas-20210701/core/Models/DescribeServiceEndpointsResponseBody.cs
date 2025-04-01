b1b6335b0c9a398ccac60984cfdb67ff05e63bae
sdk-team <sdk-team@alibabacloud.com>
2025-04-01 08:44:09 +0000

    Update API DescribeGroupEndpoints: update response parameters Body.Endpoints' type has changed.

--- eas-20210701/core/Models/DescribeServiceEndpointsResponseBody.cs
+++ eas-20210701/core/Models/DescribeServiceEndpointsResponseBody.cs
@@ -24,74 +24,28 @@ namespace AlibabaCloud.SDK.Eas20210701.Models
         /// </summary>
         [NameInMap("Endpoints")]
         [Validation(Required=false)]
-        public DescribeServiceEndpointsResponseBodyEndpoints Endpoints { get; set; }
+        public List<DescribeServiceEndpointsResponseBodyEndpoints> Endpoints { get; set; }
         public class DescribeServiceEndpointsResponseBodyEndpoints : TeaModel {
-            /// <summary>
-            /// <para>The backend access ID, which varies based on the value of the EndpointType parameter.</para>
-            /// <list type="bullet">
-            /// <item><description>If you set EndpointType to DefaultGateway, the value of this parameter is default.</description></item>
-            /// <item><description>If you set EndpointType to PrivateGateway, the value of this parameter is the ID of the dedicated gateway.</description></item>
-            /// <item><description>If you set EndpointType to Nlb, the value of this parameter is the ID of the NLB instance.</description></item>
-            /// <item><description>If you set EndpointType to Nacos, the value of this parameter is the ID of the Nacos instance.</description></item>
-            /// </list>
-            /// 
-            /// <b>Example:</b>
-            /// <para>nlb-5q4sp7u6oorkha****</para>
-            /// </summary>
             [NameInMap("BackendId")]
             [Validation(Required=false)]
             public string BackendId { get; set; }
 
-            /// <summary>
-            /// <para>The service endpoint type. Valid values:</para>
-            /// <list type="bullet">
-            /// <item><description>DefaultGateway: the shared gateway.</description></item>
-            /// <item><description>PrivateGateway: the dedicated gateway.</description></item>
-            /// <item><description>Nlb: Associate the service with the Network Load Balancer (NLB) instance.</description></item>
-            /// <item><description>Nacos: Associate the service with the Nacos instance.</description></item>
-            /// </list>
-            /// 
-            /// <b>Example:</b>
-            /// <para>Nlb</para>
-            /// </summary>
             [NameInMap("EndpointType")]
             [Validation(Required=false)]
             public string EndpointType { get; set; }
 
-            /// <summary>
-            /// <para>The public endpoints.</para>
-            /// </summary>
             [NameInMap("InternetEndpoints")]
             [Validation(Required=false)]
             public List<string> InternetEndpoints { get; set; }
 
-            /// <summary>
-            /// <para>The internal endpoints.</para>
-            /// </summary>
             [NameInMap("IntranetEndpoints")]
             [Validation(Required=false)]
             public List<string> IntranetEndpoints { get; set; }
 
-            /// <summary>
-            /// <para>The path type. Valid values:</para>
-            /// <list type="bullet">
-            /// <item><description>Group: the path of the service group.</description></item>
-            /// <item><description>Service: the path of the service.</description></item>
-            /// </list>
-            /// 
-            /// <b>Example:</b>
-            /// <para>Service</para>
-            /// </summary>
             [NameInMap("PathType")]
             [Validation(Required=false)]
             public string PathType { get; set; }
 
-            /// <summary>
-            /// <para>The port number. This parameter takes effect only when you associate the service with an NLB or Nacos instance.</para>
-            /// 
-            /// <b>Example:</b>
-            /// <para>9090</para>
-            /// </summary>
             [NameInMap("Port")]
             [Validation(Required=false)]
             public int? Port { get; set; }
