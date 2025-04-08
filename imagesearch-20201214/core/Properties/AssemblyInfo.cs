1041bde8e1ad4a1c79b0c1145305211a71f30b83
sdk-team <sdk-team@alibabacloud.com>
2025-04-08 07:13:44 +0000

    Generated csharp 2020-12-14 for ImageSearch.

--- imagesearch-20201214/core/Properties/AssemblyInfo.cs
+++ imagesearch-20201214/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5ead1090-1443-11f0-af3c-1396745880cd")]
+[assembly: Guid("9edbef60-1448-11f0-af91-ab7b1a1281cd")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.1.0.0")]
-[assembly: AssemblyFileVersion("4.1.0.0")]
+[assembly: AssemblyVersion("4.2.0.0")]
+[assembly: AssemblyFileVersion("4.2.0.0")]
