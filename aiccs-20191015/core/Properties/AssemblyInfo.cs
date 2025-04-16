8e8e62efad263fd8e191e726f71d0dbf1cc55132
sdk-team <sdk-team@alibabacloud.com>
2025-04-16 12:37:14 +0000

    Generated csharp 2019-10-15 for aiccs.

--- aiccs-20191015/core/Properties/AssemblyInfo.cs
+++ aiccs-20191015/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("68017da0-1a1c-11f0-860c-eb2c8470b17a")]
+[assembly: Guid("ad0a57d0-1abe-11f0-8549-7bfc833b166c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
