1eef7df843096fef52c866cce8b8ed48a6151294
sdk-team <sdk-team@alibabacloud.com>
2025-06-11 17:29:14 +0000

    Generated csharp 2025-03-10 for DlfNext.

--- dlfnext-20250310/core/Properties/AssemblyInfo.cs
+++ dlfnext-20250310/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("af42b860-44ff-11f0-af5d-ad791300276c")]
+[assembly: Guid("745f2eb0-46e7-11f0-8de5-3965d80a96d9")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
