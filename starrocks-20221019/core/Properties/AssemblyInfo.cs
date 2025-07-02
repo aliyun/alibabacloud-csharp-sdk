70920ca952c5b7fae4a0da6826a0428613750f02
sdk-team <sdk-team@alibabacloud.com>
2025-07-02 02:33:02 +0000

    Generated csharp 2022-10-19 for starrocks.

--- starrocks-20221019/core/Properties/AssemblyInfo.cs
+++ starrocks-20221019/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0f06bc20-55d5-11f0-9d6e-719aa348a338")]
+[assembly: Guid("8341bf00-56ec-11f0-b920-39f041b34b6a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
