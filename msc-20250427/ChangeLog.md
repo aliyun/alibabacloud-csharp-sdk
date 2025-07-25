2f7d41c349454be94436d1dc6053039ef13c61dc
sdk-team <sdk-team@alibabacloud.com>
2025-07-25 07:23:51 +0000

    Generated csharp 2025-04-27 for Msc.

---
+++ msc-20250427/ChangeLog.md
@@ -0,0 +1,3 @@
+2025-07-25 Version: 1.0.0
+- Generated csharp 2025-04-27 for Msc.
+
---
+++ msc-20250427/core/Client.cs
@@ -0,0 +1,40 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+using Tea;
+using Tea.Utils;
+
+
+namespace AlibabaCloud.SDK.Msc20250427
+{
+    public class Client : AlibabaCloud.OpenApiClient.Client
+    {
+
+        public Client(AlibabaCloud.OpenApiClient.Models.Config config): base(config)
+        {
+            this._endpointRule = "";
+            CheckConfig(config);
+            this._endpoint = GetEndpoint("msc", _regionId, _endpointRule, _network, _suffix, _endpointMap, _endpoint);
+        }
+
+
+        public string GetEndpoint(string productId, string regionId, string endpointRule, string network, string suffix, Dictionary<string, string> endpointMap, string endpoint)
+        {
+            if (!AlibabaCloud.TeaUtil.Common.Empty(endpoint))
+            {
+                return endpoint;
+            }
+            if (!AlibabaCloud.TeaUtil.Common.IsUnset(endpointMap) && !AlibabaCloud.TeaUtil.Common.Empty(endpointMap.Get(regionId)))
+            {
+                return endpointMap.Get(regionId);
+            }
+            return AlibabaCloud.EndpointUtil.Common.GetEndpointRules(productId, regionId, endpointRule, network, suffix);
+        }
+
+    }
+}
