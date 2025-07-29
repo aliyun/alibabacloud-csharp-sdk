90f18fff669e4da79cb509dc39a39ebf13bcc79d
sdk-team <sdk-team@alibabacloud.com>
2025-07-29 03:18:47 +0000

    Generated csharp 2024-08-01 for QuanMiaoLightApp.

--- quanmiaolightapp-20240801/core/Properties/AssemblyInfo.cs
+++ quanmiaolightapp-20240801/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5674ab60-5767-11f0-a422-694e143ab553")]
+[assembly: Guid("57af5520-6c2a-11f0-84ec-c1561cc5ea9f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.8.3.0")]
-[assembly: AssemblyFileVersion("2.8.3.0")]
+[assembly: AssemblyVersion("2.9.0.0")]
+[assembly: AssemblyFileVersion("2.9.0.0")]
