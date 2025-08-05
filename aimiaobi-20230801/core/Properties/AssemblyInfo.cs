3a89a66d90b7aa7a8121a7315590b9117a60e849
sdk-team <sdk-team@alibabacloud.com>
2025-08-05 00:47:20 +0000

    Generated csharp 2023-08-01 for AiMiaoBi.

--- aimiaobi-20230801/core/Properties/AssemblyInfo.cs
+++ aimiaobi-20230801/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a228ff40-7155-11f0-ac40-cfab0cdd1a35")]
+[assembly: Guid("4c7de430-7195-11f0-9e78-1bb4c73bb5e6")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.33.0.0")]
-[assembly: AssemblyFileVersion("1.33.0.0")]
+[assembly: AssemblyVersion("1.33.1.0")]
+[assembly: AssemblyFileVersion("1.33.1.0")]
