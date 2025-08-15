dc11fc219f44c6710c0d0c8b3cf5758be7958b53
sdk-team <sdk-team@alibabacloud.com>
2025-08-15 09:10:11 +0000

    Generated csharp 2023-09-30 for eds-aic.

--- eds-aic-20230930/core/Properties/AssemblyInfo.cs
+++ eds-aic-20230930/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6ef66900-747a-11f0-aa4b-873251cdc9ef")]
+[assembly: Guid("41403b70-79b7-11f0-88d6-1b38647ef04a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.10.4.0")]
-[assembly: AssemblyFileVersion("4.10.4.0")]
+[assembly: AssemblyVersion("4.11.0.0")]
+[assembly: AssemblyFileVersion("4.11.1.0")]
