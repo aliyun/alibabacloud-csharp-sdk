76b88b96330b8d97ab0a083d541198e138b88557
sdk-team <sdk-team@alibabacloud.com>
2025-08-22 02:25:45 +0000

    Generated csharp 2020-01-01 for ddoscoo.

--- ddoscoo-20200101/core/Properties/AssemblyInfo.cs
+++ ddoscoo-20200101/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("46dee7d0-7eb1-11f0-a8b6-151c5df3e2ee")]
+[assembly: Guid("ea1f7b10-7efe-11f0-8bc0-3d55698d5801")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.1.0")]
-[assembly: AssemblyFileVersion("3.0.2.0")]
+[assembly: AssemblyVersion("3.0.2.0")]
+[assembly: AssemblyFileVersion("3.0.3.0")]
