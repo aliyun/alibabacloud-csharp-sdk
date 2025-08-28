b7a659af05deb61fbdfab5243981f23bf1a15958
sdk-team <sdk-team@alibabacloud.com>
2025-08-28 03:18:41 +0000

    Delete API ListHotlineTransferNumber.

--- dyvmsapi-20170525/core/Properties/AssemblyInfo.cs
+++ dyvmsapi-20170525/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("686f4bd0-8adb-11ef-8166-1fc264aca37a")]
+[assembly: Guid("4d597010-83bd-11f0-9d1f-b981c8a5479d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.2.1.0")]
-[assembly: AssemblyFileVersion("3.2.1.0")]
+[assembly: AssemblyVersion("4.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
