06de4aa44b373721cd9b5aafdcf53e153115c476
sdk-team <sdk-team@alibabacloud.com>
2025-09-12 09:15:05 +0000

    Generated csharp 2023-06-30 for dataphin-public.

--- dataphin-public-20230630/core/Properties/AssemblyInfo.cs
+++ dataphin-public-20230630/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d8e19ca0-84a5-11f0-8598-0935e1512125")]
+[assembly: Guid("9b5f8bd0-8fb8-11f0-bf64-25c388ce1afa")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.1.0")]
