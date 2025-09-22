28c529e6ccf7aef2f4b93b8fceada584fc979e32
sdk-team <sdk-team@alibabacloud.com>
2025-09-22 02:01:53 +0000

    Generated csharp 2021-09-10 for xtee.

--- xtee-20210910/core/Properties/AssemblyInfo.cs
+++ xtee-20210910/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6ac5cf70-957b-11f0-9573-25d331677300")]
+[assembly: Guid("afdb7730-9757-11f0-93a6-7d04ea6d000c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.1.0")]
