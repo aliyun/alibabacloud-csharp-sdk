ccf82f287b05d51243f8ca7a377341dc899d5fb9
sdk-team <sdk-team@alibabacloud.com>
2025-09-22 13:05:04 +0000

    Update API ImageQueueModeration: add response parameters Body.Data.Frame.

--- green-20220302/core/Properties/AssemblyInfo.cs
+++ green-20220302/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("03758b20-979d-11f0-8675-7b1a383c2b36")]
+[assembly: Guid("5606f790-97b4-11f0-b0a4-33b4d0204836")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.22.1.0")]
-[assembly: AssemblyFileVersion("2.23.0.0")]
+[assembly: AssemblyVersion("3.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
