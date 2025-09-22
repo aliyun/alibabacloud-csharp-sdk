6ccad09e7b6b7c25118f89a5a3aeac5a751d2c09
sdk-team <sdk-team@alibabacloud.com>
2025-09-22 11:41:49 +0000

    Generated csharp 2025-07-07 for AnyTrans.

--- anytrans-20250707/core/Models/SubmitDocTranslateTaskShrinkRequest.cs
+++ anytrans-20250707/core/Models/SubmitDocTranslateTaskShrinkRequest.cs
@@ -53,7 +53,7 @@ namespace AlibabaCloud.SDK.AnyTrans20250707.Models
         /// <para>This parameter is required.</para>
         /// 
         /// <b>Example:</b>
-        /// <para><a href="https://xxx-hangzhou.aliyuncs.com/docs/tmp/%E6%A0%B7%E4%BE%8B_%E6%97%A0%E5%9B%BE.pdf?OSSAccessKeyId=LTAI5tBVMtznbk7xyCa56gof&Expires=1755883153&Signature=H23MpMZqFVPGTPCU2GtFqOFfmII%3D">https://xxx-hangzhou.aliyuncs.com/docs/tmp/%E6%A0%B7%E4%BE%8B_%E6%97%A0%E5%9B%BE.pdf?OSSAccessKeyId=LTAI5tBVMtznbk7xyCa56gof&amp;Expires=1755883153&amp;Signature=H23MpMZqFVPGTPCU2GtFqOFfmII%3D</a></para>
+        /// <para><a href="https://xxx-hangzhou.aliyuncs.com/docs/tmp/%E6%A0%B7%E4%BE%8B_%E6%97%A0%E5%9B%BE.pdf">https://xxx-hangzhou.aliyuncs.com/docs/tmp/%E6%A0%B7%E4%BE%8B_%E6%97%A0%E5%9B%BE.pdf</a></para>
         /// </summary>
         [NameInMap("text")]
         [Validation(Required=false)]
--- anytrans-20250707/core/Properties/AssemblyInfo.cs
+++ anytrans-20250707/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a5edb500-978c-11f0-af0e-6b3eb7c4114b")]
+[assembly: Guid("b9d63ad0-97a8-11f0-a393-7d61e7179cc7")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.1.4.0")]
+[assembly: AssemblyFileVersion("1.1.5.0")]
