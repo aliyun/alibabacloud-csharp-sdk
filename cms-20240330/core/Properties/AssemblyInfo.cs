30e83ee4cdfcd87bcb80578e9aef02b710d5530d
sdk-team <sdk-team@alibabacloud.com>
2025-09-24 05:40:46 +0000

    Generated csharp 2024-03-30 for Cms.

--- cms-20240330/core/Properties/AssemblyInfo.cs
+++ cms-20240330/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e1ad7670-8e68-11f0-86e9-0747b9a2c52b")]
+[assembly: Guid("a96e94b0-9908-11f0-a815-df281845fe3f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("3.1.0.0")]
+[assembly: AssemblyFileVersion("3.1.1.0")]
