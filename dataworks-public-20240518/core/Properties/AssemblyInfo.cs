ca237e5813032eab2c6980c9f6691c74195b546a
sdk-team <sdk-team@alibabacloud.com>
2025-11-26 04:06:34 +0000

    Generated csharp 2024-05-18 for dataworks-public.

--- dataworks-public-20240518/core/Properties/AssemblyInfo.cs
+++ dataworks-public-20240518/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c16c30f0-ca21-11f0-9c06-a9042c8ee156")]
+[assembly: Guid("d2aa58a0-ca7c-11f0-b58f-c93ac4a25228")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.6.0.0")]
-[assembly: AssemblyFileVersion("4.6.1.0")]
+[assembly: AssemblyVersion("4.6.1.0")]
+[assembly: AssemblyFileVersion("4.6.2.0")]
