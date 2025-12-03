0d575b06f52b4900768b663181f275cf5ec2a082
sdk-team <sdk-team@alibabacloud.com>
2025-12-03 10:03:47 +0000

    Update SDK.

--- msc-20250427/core/Properties/AssemblyInfo.cs
+++ msc-20250427/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f64392b0-6927-11f0-8159-995f4801e824")]
+[assembly: Guid("d62fa330-d02c-11f0-a879-9bf2bb4849c4")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
