bf8ef5815b1ad8ff12fca6fa4d2b4c8779516924
sdk-team <sdk-team@alibabacloud.com>
2025-12-05 09:03:24 +0000

    Generated csharp 2017-01-15 for HBase.

--- hbase-20170115/core/Properties/AssemblyInfo.cs
+++ hbase-20170115/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("074539b0-d025-11f0-9179-19cc9705c368")]
+[assembly: Guid("4f48b230-d1b7-11f0-9cfa-c334b46057d7")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
