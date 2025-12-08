613074012b424abfa8bcdeba7732cac2d459072d
sdk-team <sdk-team@alibabacloud.com>
2025-12-08 07:08:33 +0000

    Generated csharp 2025-05-07 for RdsAi.

--- rdsai-20250507/core/Properties/AssemblyInfo.cs
+++ rdsai-20250507/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e1702630-d18e-11f0-bd38-aff63cb0e5e2")]
+[assembly: Guid("463e96b0-d404-11f0-9645-5d92b7cdc0d5")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
