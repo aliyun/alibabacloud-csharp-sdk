2d9bcfb24e95e893020504597dfb199e309a821f
sdk-team <sdk-team@alibabacloud.com>
2025-12-08 07:48:49 +0000

    Generated csharp 2020-07-10 for mPaaS.

--- mpaas-20200710/ChangeLog.md
+++ mpaas-20200710/ChangeLog.md
@@ -1,3 +1,6 @@
+2025-12-08 Version: 1.0.1
+- Generated csharp 2020-07-10 for mPaaS.
+
 2025-08-25 Version: 1.0.0
 - Generated csharp 2020-07-10 for mPaaS.
 
--- mpaas-20200710/core/Properties/AssemblyInfo.cs
+++ mpaas-20200710/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fe31bab0-8164-11f0-a356-a326cd204039")]
+[assembly: Guid("ce132240-d409-11f0-a930-8f267c18f378")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
