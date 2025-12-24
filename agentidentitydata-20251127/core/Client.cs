91acff76686d5ab0c6f60341877ed5530cadee85
sdk-team <sdk-team@alibabacloud.com>
2025-12-24 09:20:50 +0000

    Generated csharp 2025-11-27 for AgentIdentityData.

--- agentidentitydata-20251127/core/Client.cs
+++ agentidentitydata-20251127/core/Client.cs
@@ -18,7 +18,7 @@ namespace AlibabaCloud.SDK.AgentIdentityData20251127
 
         public Client(AlibabaCloud.OpenApiClient.Models.Config config): base(config)
         {
-            this._endpointRule = "";
+            this._endpointRule = "regional";
             CheckConfig(config);
             this._endpoint = GetEndpoint("agentidentitydata", _regionId, _endpointRule, _network, _suffix, _endpointMap, _endpoint);
         }
--- agentidentitydata-20251127/core/Properties/AssemblyInfo.cs
+++ agentidentitydata-20251127/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b1770240-dcc5-11f0-ab01-f92a8e56c87c")]
+[assembly: Guid("29834910-e0a9-11f0-8bf8-b38643e0c685")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
