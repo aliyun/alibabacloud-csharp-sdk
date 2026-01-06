7e444739ad6531ded07e0098743f25f3927e875d
sdk-team <sdk-team@alibabacloud.com>
2026-01-06 17:43:20 +0000

    Generated csharp 2020-01-16 for DAS.

--- das-20200116/core/Properties/AssemblyInfo.cs
+++ das-20200116/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("43967890-e022-11f0-be68-d368f6ebfee0")]
+[assembly: Guid("5ff284b0-eb22-11f0-a4f2-df44bc97e103")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.8.0.0")]
-[assembly: AssemblyFileVersion("2.8.1.0")]
+[assembly: AssemblyVersion("2.8.1.0")]
+[assembly: AssemblyFileVersion("2.8.2.0")]
