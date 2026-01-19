d9ebdb14e82f41f9581e95de408e4b2925629865
sdk-team <sdk-team@alibabacloud.com>
2026-01-19 02:30:54 +0000

    Generated csharp 2024-04-02 for acc.

--- acc-20240402/core/Properties/AssemblyInfo.cs
+++ acc-20240402/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("79370160-939a-11f0-9a60-f53b8f60e97f")]
+[assembly: Guid("61881270-f4de-11f0-befd-ab6c627078f1")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
