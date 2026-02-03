e00877b3be2ffea9c653be02b0882c059875e506
sdk-team <sdk-team@alibabacloud.com>
2026-02-03 04:48:10 +0000

    Generated csharp 2019-12-09 for Yundun-bastionhost.

--- yundun-bastionhost-20191209/core/Properties/AssemblyInfo.cs
+++ yundun-bastionhost-20191209/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("134fb170-f6ec-11f0-ab36-572b7f56b7fa")]
+[assembly: Guid("e1561c30-00ba-11f1-95d7-ffaf15d918f2")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.1.0")]
-[assembly: AssemblyFileVersion("1.4.2.0")]
+[assembly: AssemblyVersion("1.4.2.0")]
+[assembly: AssemblyFileVersion("1.4.3.0")]
