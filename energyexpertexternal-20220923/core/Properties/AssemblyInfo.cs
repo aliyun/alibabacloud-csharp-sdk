f5e92e93a017117c6a948bedfb340c9663a0de5a
sdk-team <sdk-team@alibabacloud.com>
2026-02-14 02:39:27 +0000

    Generated csharp 2022-09-23 for energyExpertExternal.

--- energyexpertexternal-20220923/core/Properties/AssemblyInfo.cs
+++ energyexpertexternal-20220923/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("13595d40-08e6-11f1-b662-012761fadd16")]
+[assembly: Guid("db14c7e0-094d-11f1-8ef5-95ba6f3cd0f7")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.4.3.0")]
-[assembly: AssemblyFileVersion("4.5.0.0")]
+[assembly: AssemblyVersion("4.5.0.0")]
+[assembly: AssemblyFileVersion("4.5.1.0")]
