45dd9107812834806d4a1ed1853a1519402f8e92
sdk-team <sdk-team@alibabacloud.com>
2026-02-25 17:25:32 +0000

    Generated csharp 2022-01-12 for PaiStudio.

--- paistudio-20220112/core/Properties/AssemblyInfo.cs
+++ paistudio-20220112/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("49b020a0-1227-11f1-9c2b-319de47046d5")]
+[assembly: Guid("b97ad980-126c-11f1-a078-5f03882c43af")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.2.5.0")]
-[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: AssemblyVersion("2.4.0.0")]
+[assembly: AssemblyFileVersion("2.4.1.0")]
