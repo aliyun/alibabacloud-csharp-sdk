ea115cadb17a3c578b04a683c4e48e5f83da0b7a
sdk-team <sdk-team@alibabacloud.com>
2026-02-27 12:01:20 +0000

    Delete API ActivateDevice.

--- wyota-20210420/core/Properties/AssemblyInfo.cs
+++ wyota-20210420/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a0752350-6655-11f0-b2a4-ab393d99e25c")]
+[assembly: Guid("91e0ebe0-13d3-11f1-9033-5596af6128d2")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyVersion("2.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
