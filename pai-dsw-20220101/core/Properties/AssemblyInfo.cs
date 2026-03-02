19641b64f1506825a75d8d8c0edf149ce654b104
sdk-team <sdk-team@alibabacloud.com>
2026-03-02 07:28:10 +0000

    Generated csharp 2022-01-01 for pai-dsw.

--- pai-dsw-20220101/core/Properties/AssemblyInfo.cs
+++ pai-dsw-20220101/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6f002a60-1607-11f1-b002-15b2a3a22440")]
+[assembly: Guid("ecab3a30-1608-11f1-be8d-0b919f88dd19")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.3.5.0")]
-[assembly: AssemblyFileVersion("2.3.6.0")]
+[assembly: AssemblyVersion("2.3.6.0")]
+[assembly: AssemblyFileVersion("2.3.7.0")]
