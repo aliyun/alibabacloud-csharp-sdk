5eb7538573e3a5db0926f504609affa11ac20a11
sdk-team <sdk-team@alibabacloud.com>
2026-03-17 12:51:45 +0000

    Generated csharp 2025-07-07 for AnyTrans.

--- anytrans-20250707/core/Properties/AssemblyInfo.cs
+++ anytrans-20250707/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("754c7620-21f7-11f1-bc6b-97570d815d08")]
+[assembly: Guid("a39d2d00-21ff-11f1-ba18-e9aa20d2ddae")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("2.0")]
-[assembly: AssemblyFileVersion("2.1.3.0")]
+[assembly: AssemblyFileVersion("2.1.4.0")]
