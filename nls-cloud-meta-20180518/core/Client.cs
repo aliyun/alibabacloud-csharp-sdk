85f32ffac176fad3aa56bca51ae77bfdd24157d4
sdk-team <sdk-team@alibabacloud.com>
2026-03-20 17:37:20 +0000

    Generated csharp 2018-05-18 for nls-cloud-meta.

---
+++ nls-cloud-meta-20180518/core/Client.cs
@@ -0,0 +1,143 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+using Tea;
+using Tea.Utils;
+
+using AlibabaCloud.SDK.Nls_cloud_meta20180518.Models;
+
+namespace AlibabaCloud.SDK.Nls_cloud_meta20180518
+{
+    public class Client : AlibabaCloud.OpenApiClient.Client
+    {
+
+        public Client(AlibabaCloud.OpenApiClient.Models.Config config): base(config)
+        {
+            this._endpointRule = "";
+            CheckConfig(config);
+            this._endpoint = GetEndpoint("nls-cloud-meta", _regionId, _endpointRule, _network, _suffix, _endpointMap, _endpoint);
+        }
+
+
+        public string GetEndpoint(string productId, string regionId, string endpointRule, string network, string suffix, Dictionary<string, string> endpointMap, string endpoint)
+        {
+            if (!AlibabaCloud.TeaUtil.Common.Empty(endpoint))
+            {
+                return endpoint;
+            }
+            if (!AlibabaCloud.TeaUtil.Common.IsUnset(endpointMap) && !AlibabaCloud.TeaUtil.Common.Empty(endpointMap.Get(regionId)))
+            {
+                return endpointMap.Get(regionId);
+            }
+            return AlibabaCloud.EndpointUtil.Common.GetEndpointRules(productId, regionId, endpointRule, network, suffix);
+        }
+
+        /// <term><b>Summary:</b></term>
+        /// <summary>
+        /// <para>创建Token</para>
+        /// </summary>
+        /// 
+        /// <param name="headers">
+        /// map
+        /// </param>
+        /// <param name="runtime">
+        /// runtime options for this request RuntimeOptions
+        /// </param>
+        /// 
+        /// <returns>
+        /// CreateTokenResponse
+        /// </returns>
+        public CreateTokenResponse CreateTokenWithOptions(Dictionary<string, string> headers, AlibabaCloud.TeaUtil.Models.RuntimeOptions runtime)
+        {
+            AlibabaCloud.OpenApiClient.Models.OpenApiRequest req = new AlibabaCloud.OpenApiClient.Models.OpenApiRequest
+            {
+                Headers = headers,
+            };
+            AlibabaCloud.OpenApiClient.Models.Params params_ = new AlibabaCloud.OpenApiClient.Models.Params
+            {
+                Action = "CreateToken",
+                Version = "2018-05-18",
+                Protocol = "HTTPS",
+                Pathname = "/pop/2018-05-18/tokens",
+                Method = "POST",
+                AuthType = "AK",
+                Style = "ROA",
+                ReqBodyType = "json",
+                BodyType = "json",
+            };
+            return TeaModel.ToObject<CreateTokenResponse>(CallApi(params_, req, runtime));
+        }
+
+        /// <term><b>Summary:</b></term>
+        /// <summary>
+        /// <para>创建Token</para>
+        /// </summary>
+        /// 
+        /// <param name="headers">
+        /// map
+        /// </param>
+        /// <param name="runtime">
+        /// runtime options for this request RuntimeOptions
+        /// </param>
+        /// 
+        /// <returns>
+        /// CreateTokenResponse
+        /// </returns>
+        public async Task<CreateTokenResponse> CreateTokenWithOptionsAsync(Dictionary<string, string> headers, AlibabaCloud.TeaUtil.Models.RuntimeOptions runtime)
+        {
+            AlibabaCloud.OpenApiClient.Models.OpenApiRequest req = new AlibabaCloud.OpenApiClient.Models.OpenApiRequest
+            {
+                Headers = headers,
+            };
+            AlibabaCloud.OpenApiClient.Models.Params params_ = new AlibabaCloud.OpenApiClient.Models.Params
+            {
+                Action = "CreateToken",
+                Version = "2018-05-18",
+                Protocol = "HTTPS",
+                Pathname = "/pop/2018-05-18/tokens",
+                Method = "POST",
+                AuthType = "AK",
+                Style = "ROA",
+                ReqBodyType = "json",
+                BodyType = "json",
+            };
+            return TeaModel.ToObject<CreateTokenResponse>(await CallApiAsync(params_, req, runtime));
+        }
+
+        /// <term><b>Summary:</b></term>
+        /// <summary>
+        /// <para>创建Token</para>
+        /// </summary>
+        /// 
+        /// <returns>
+        /// CreateTokenResponse
+        /// </returns>
+        public CreateTokenResponse CreateToken()
+        {
+            AlibabaCloud.TeaUtil.Models.RuntimeOptions runtime = new AlibabaCloud.TeaUtil.Models.RuntimeOptions();
+            Dictionary<string, string> headers = new Dictionary<string, string>(){};
+            return CreateTokenWithOptions(headers, runtime);
+        }
+
+        /// <term><b>Summary:</b></term>
+        /// <summary>
+        /// <para>创建Token</para>
+        /// </summary>
+        /// 
+        /// <returns>
+        /// CreateTokenResponse
+        /// </returns>
+        public async Task<CreateTokenResponse> CreateTokenAsync()
+        {
+            AlibabaCloud.TeaUtil.Models.RuntimeOptions runtime = new AlibabaCloud.TeaUtil.Models.RuntimeOptions();
+            Dictionary<string, string> headers = new Dictionary<string, string>(){};
+            return await CreateTokenWithOptionsAsync(headers, runtime);
+        }
+
+    }
+}
