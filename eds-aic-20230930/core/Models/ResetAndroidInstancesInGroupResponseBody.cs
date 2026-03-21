98ad1447ffab4c1b7fe35fcf2b823e55b714a001
sdk-team <sdk-team@alibabacloud.com>
2026-03-21 17:19:16 +0000

    Update API RebootAndroidInstancesInGroup: add response parameters Body.Tasks.

--- eds-aic-20230930/core/Models/ResetAndroidInstancesInGroupResponseBody.cs
+++ eds-aic-20230930/core/Models/ResetAndroidInstancesInGroupResponseBody.cs
@@ -19,6 +19,42 @@ namespace AlibabaCloud.SDK.Eds_aic20230930.Models
         [Validation(Required=false)]
         public string RequestId { get; set; }
 
+        [NameInMap("Tasks")]
+        [Validation(Required=false)]
+        public ResetAndroidInstancesInGroupResponseBodyTasks Tasks { get; set; }
+        public class ResetAndroidInstancesInGroupResponseBodyTasks : TeaModel {
+            [NameInMap("ChildTasks")]
+            [Validation(Required=false)]
+            public List<ResetAndroidInstancesInGroupResponseBodyTasksChildTasks> ChildTasks { get; set; }
+            public class ResetAndroidInstancesInGroupResponseBodyTasksChildTasks : TeaModel {
+                /// <summary>
+                /// <b>Example:</b>
+                /// <para>acp-xxxx</para>
+                /// </summary>
+                [NameInMap("InstanceId")]
+                [Validation(Required=false)]
+                public string InstanceId { get; set; }
+
+                /// <summary>
+                /// <b>Example:</b>
+                /// <para>t-xxxx</para>
+                /// </summary>
+                [NameInMap("TaskId")]
+                [Validation(Required=false)]
+                public string TaskId { get; set; }
+
+            }
+
+            /// <summary>
+            /// <b>Example:</b>
+            /// <para>t-xxxx</para>
+            /// </summary>
+            [NameInMap("ParentTaskId")]
+            [Validation(Required=false)]
+            public string ParentTaskId { get; set; }
+
+        }
+
     }
 
 }
--- eds-aic-20230930/core/Models/UpdateInstanceImageResponseBody.cs
+++ eds-aic-20230930/core/Models/UpdateInstanceImageResponseBody.cs
@@ -25,6 +25,42 @@ namespace AlibabaCloud.SDK.Eds_aic20230930.Models
         [Validation(Required=false)]
         public string TaskId { get; set; }
 
+        [NameInMap("Tasks")]
+        [Validation(Required=false)]
+        public UpdateInstanceImageResponseBodyTasks Tasks { get; set; }
+        public class UpdateInstanceImageResponseBodyTasks : TeaModel {
+            [NameInMap("ChildTasks")]
+            [Validation(Required=false)]
+            public List<UpdateInstanceImageResponseBodyTasksChildTasks> ChildTasks { get; set; }
+            public class UpdateInstanceImageResponseBodyTasksChildTasks : TeaModel {
+                /// <summary>
+                /// <b>Example:</b>
+                /// <para>acp-xxxx</para>
+                /// </summary>
+                [NameInMap("InstanceId")]
+                [Validation(Required=false)]
+                public string InstanceId { get; set; }
+
+                /// <summary>
+                /// <b>Example:</b>
+                /// <para>t-xxxx</para>
+                /// </summary>
+                [NameInMap("TaskId")]
+                [Validation(Required=false)]
+                public string TaskId { get; set; }
+
+            }
+
+            /// <summary>
+            /// <b>Example:</b>
+            /// <para>t-xxxx</para>
+            /// </summary>
+            [NameInMap("ParentTaskId")]
+            [Validation(Required=false)]
+            public string ParentTaskId { get; set; }
+
+        }
+
     }
 
 }
