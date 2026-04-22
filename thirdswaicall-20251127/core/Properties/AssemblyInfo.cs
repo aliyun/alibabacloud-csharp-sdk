edc549b3ecba789f9a0cbe59a30a8272b3ce1fbf
sdk-team <sdk-team@alibabacloud.com>
2026-04-22 08:51:10 +0000

    Generated csharp 2025-11-27 for ThirdswAicall.

--- thirdswaicall-20251127/core/Properties/AssemblyInfo.cs
+++ thirdswaicall-20251127/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f0e91320-1846-11f1-8c1a-afa93962fdd1")]
+[assembly: Guid("f379f9e0-3e27-11f1-85de-574d4d560268")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
