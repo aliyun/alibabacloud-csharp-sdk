a870259e9be7d497f61ae28c3cfb2de7650fdbc8
sdk-team <sdk-team@alibabacloud.com>
2026-04-28 03:51:11 +0000

    Generated csharp 2024-05-29 for Searchplat.

--- searchplat-20240529/core/Properties/AssemblyInfo.cs
+++ searchplat-20240529/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("39445ef0-42b2-11f1-ae8c-99e090a39cd0")]
+[assembly: Guid("a34412d0-42b4-11f1-9157-5fcf04ea241a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.4.0.0")]
-[assembly: AssemblyFileVersion("2.5.0.0")]
+[assembly: AssemblyVersion("2.5.0.0")]
+[assembly: AssemblyFileVersion("2.5.1.0")]
