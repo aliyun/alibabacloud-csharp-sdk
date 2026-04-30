5ce63ff9d01fa3de983e31801fb91a8e2ac257e0
sdk-team <sdk-team@alibabacloud.com>
2026-04-30 08:16:08 +0000

    Delete API DocumentParseTestApi.

--- documentparseservice-20260414/core/Properties/AssemblyInfo.cs
+++ documentparseservice-20260414/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a8c21fa0-424f-11f1-9aa7-9da76a416cbb")]
+[assembly: Guid("532cd5c0-446c-11f1-850a-0f482c4be528")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("2.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
