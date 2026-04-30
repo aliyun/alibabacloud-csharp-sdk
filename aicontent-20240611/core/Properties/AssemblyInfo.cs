7304498ba7f31b0b3ea0b5500108d8b4eb99ba3b
sdk-team <sdk-team@alibabacloud.com>
2026-04-30 06:11:33 +0000

    Generated csharp 20240611 for AiContent.

--- aicontent-20240611/core/Properties/AssemblyInfo.cs
+++ aicontent-20240611/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("078c92e0-4458-11f1-9e46-2fdc919ecf37")]
+[assembly: Guid("e1d42c40-445a-11f1-9f18-0f6756b0e601")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("7.3.0.0")]
-[assembly: AssemblyFileVersion("7.4.0.0")]
+[assembly: AssemblyVersion("7.4.0.0")]
+[assembly: AssemblyFileVersion("7.4.1.0")]
