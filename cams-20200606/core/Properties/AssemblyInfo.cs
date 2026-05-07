5378df59f724ad64a876b9117d84537afbf6b9c2
sdk-team <sdk-team@alibabacloud.com>
2026-05-07 03:25:00 +0000

    Generated csharp 2020-06-06 for cams.

--- cams-20200606/core/Properties/AssemblyInfo.cs
+++ cams-20200606/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("582cad90-4325-11f1-9107-61440ffed26a")]
+[assembly: Guid("dda897a0-49c3-11f1-9067-b58339d48133")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.1.0.0")]
+[assembly: AssemblyVersion("5.1.0.0")]
+[assembly: AssemblyFileVersion("5.1.1.0")]
