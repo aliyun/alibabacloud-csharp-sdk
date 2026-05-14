58c66b859f2262e4a875b2f6ee5b12bcdd0f32d8
sdk-team <sdk-team@alibabacloud.com>
2026-05-14 06:48:09 +0000

    Update API WebSearch: add response parameters Body.data.result.$.date.

--- maasaisearchproxy-20260424/core/Models/WebSearchResponseBody.cs
+++ maasaisearchproxy-20260424/core/Models/WebSearchResponseBody.cs
@@ -25,9 +25,51 @@ namespace AlibabaCloud.SDK.MaasAISearchProxy20260424.Models
             [Validation(Required=false)]
             public List<WebSearchResponseBodyDataResult> Result { get; set; }
             public class WebSearchResponseBodyDataResult : TeaModel {
+                /// <summary>
+                /// <b>Example:</b>
+                /// <para>1990-01-01 12:00:00</para>
+                /// </summary>
+                [NameInMap("date")]
+                [Validation(Required=false)]
+                public string Date { get; set; }
+
+                /// <summary>
+                /// <b>Example:</b>
+                /// <para>snippet</para>
+                /// </summary>
                 [NameInMap("snippet")]
                 [Validation(Required=false)]
                 public string Snippet { get; set; }
+
+                [NameInMap("source")]
+                [Validation(Required=false)]
+                public WebSearchResponseBodyDataResultSource Source { get; set; }
+                public class WebSearchResponseBodyDataResultSource : TeaModel {
+                    /// <summary>
+                    /// <b>Example:</b>
+                    /// <para>domain</para>
+                    /// </summary>
+                    [NameInMap("domain")]
+                    [Validation(Required=false)]
+                    public string Domain { get; set; }
+
+                    /// <summary>
+                    /// <b>Example:</b>
+                    /// <para>favicon</para>
+                    /// </summary>
+                    [NameInMap("favicon")]
+                    [Validation(Required=false)]
+                    public string Favicon { get; set; }
+
+                    /// <summary>
+                    /// <b>Example:</b>
+                    /// <para>name</para>
+                    /// </summary>
+                    [NameInMap("name")]
+                    [Validation(Required=false)]
+                    public string Name { get; set; }
+
+                }
 
                 /// <summary>
                 /// <b>Example:</b>
@@ -64,16 +106,6 @@ namespace AlibabaCloud.SDK.MaasAISearchProxy20260424.Models
         [NameInMap("message")]
         [Validation(Required=false)]
         public string Message { get; set; }
-
-        /// <summary>
-        /// <para>requestId</para>
-        /// 
-        /// <b>Example:</b>
-        /// <para>3b5215d417623961959166934d009a</para>
-        /// </summary>
-        [NameInMap("traceId")]
-        [Validation(Required=false)]
-        public string TraceId { get; set; }
 
     }
 
