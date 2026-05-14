1079bc600b9c56bcfc901ea430a39380b01a2b9c
sdk-team <sdk-team@alibabacloud.com>
2026-05-14 06:08:20 +0000

    Generated csharp 2024-11-11 for IQS.

--- iqs-20241111/core/Properties/AssemblyInfo.cs
+++ iqs-20241111/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("39bb9240-4d5c-11f1-be05-91c5dc857748")]
+[assembly: Guid("c6ac0810-4f5a-11f1-b601-5191bea54ffb")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.7.3.0")]
-[assembly: AssemblyFileVersion("1.9.0.0")]
+[assembly: AssemblyVersion("1.9.0.0")]
+[assembly: AssemblyFileVersion("1.9.1.0")]
