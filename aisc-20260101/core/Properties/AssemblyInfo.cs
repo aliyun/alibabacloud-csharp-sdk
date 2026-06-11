e106cfa0ab71c6ce7664f5faa24b6897c09353fb
sdk-team <sdk-team@alibabacloud.com>
2026-06-11 07:40:06 +0000

    Generated csharp 2026-01-01 for AISC.

--- aisc-20260101/core/Properties/AssemblyInfo.cs
+++ aisc-20260101/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9c5754a0-6564-11f1-8852-35e8a03a0d9b")]
+[assembly: Guid("2caaef00-6568-11f1-b645-e3a98f094d04")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
