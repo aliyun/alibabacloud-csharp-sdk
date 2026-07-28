4dafb4177d476859a321f4d6b03425fdbb322a96
sdk-team <sdk-team@alibabacloud.com>
2026-07-28 06:59:13 +0000

    Generated csharp 2025-09-09 for SfmMultiModalApp.

--- sfmmultimodalapp-20250909/core/Properties/AssemblyInfo.cs
+++ sfmmultimodalapp-20250909/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b00ccda0-8a4d-11f1-a86f-7906052a6cae")]
+[assembly: Guid("48e97700-8a51-11f1-b978-5b11487112f1")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.1.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.1.0")]
