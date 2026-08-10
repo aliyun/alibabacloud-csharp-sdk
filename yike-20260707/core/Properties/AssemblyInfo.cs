c13e6e622fda473ef8aebc5f4d81adf33b1459fb
sdk-team <sdk-team@alibabacloud.com>
2026-08-10 02:40:17 +0000

    Generated csharp 2026-07-07 for Yike.

--- yike-20260707/core/Properties/AssemblyInfo.cs
+++ yike-20260707/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7ab19b60-945f-11f1-b953-a5a436c9e070")]
+[assembly: Guid("50d01290-9464-11f1-bb6d-c1d8d6f8d64f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.1.0")]
