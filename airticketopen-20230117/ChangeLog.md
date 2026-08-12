154e829d44755a87fca41314eb58efa2f757ba55
sdk-team <sdk-team@alibabacloud.com>
2026-08-12 02:55:34 +0000

    Generated csharp 2023-01-17 for airticketOpen.

--- airticketopen-20230117/ChangeLog.md
+++ airticketopen-20230117/ChangeLog.md
@@ -1,3 +1,6 @@
+2026-08-12 Version: 6.0.2
+- Generated csharp 2023-01-17 for airticketOpen.
+
 2026-08-12 Version: 6.0.1
 - Generated csharp 2023-01-17 for airticketOpen.
 
--- airticketopen-20230117/core/Properties/AssemblyInfo.cs
+++ airticketopen-20230117/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b1b56fa0-95f5-11f1-896b-811750898de3")]
+[assembly: Guid("c6d80570-95f8-11f1-a251-471ffae7561d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("6.0.0.0")]
-[assembly: AssemblyFileVersion("6.0.1.0")]
+[assembly: AssemblyVersion("6.0.1.0")]
+[assembly: AssemblyFileVersion("6.0.2.0")]
