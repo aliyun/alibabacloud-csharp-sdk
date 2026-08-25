d28614ee7f63ace6284a4b5987bf908c6b67ff7a
sdk-team <sdk-team@alibabacloud.com>
2026-08-25 11:27:20 +0000

    Generated csharp 2015-12-15 for CS.

--- cs-20151215/core/Properties/AssemblyInfo.cs
+++ cs-20151215/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2dc1a170-a048-11f1-84b8-459dfe682df3")]
+[assembly: Guid("5acd1c60-a077-11f1-b5e8-a10e8f73873d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("7.1.1.0")]
-[assembly: AssemblyFileVersion("7.1.2.0")]
+[assembly: AssemblyVersion("7.1.2.0")]
+[assembly: AssemblyFileVersion("7.1.3.0")]
