e03b257be6adde78826ffc40e19e2bce7bb68557
sdk-team <sdk-team@alibabacloud.com>
2026-08-28 07:38:34 +0000

    Generated csharp 2026-04-01 for DtsAI.

--- dtsai-20260401/core/Properties/AssemblyInfo.cs
+++ dtsai-20260401/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("20a76440-a23a-11f1-9537-d9d02a48818a")]
+[assembly: Guid("ce473e90-a2b2-11f1-ab13-6198a7bff008")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.2.0")]
