815b5d547f395cc93db93bf3d158cddca4eb0d4b
sdk-team <sdk-team@alibabacloud.com>
2026-09-08 09:15:58 +0000

    Generated csharp 2021-01-01 for marketing_event.

--- marketing_event-20210101/core/Properties/AssemblyInfo.cs
+++ marketing_event-20210101/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e78e4530-a04e-11f1-a581-d5312da0d82e")]
+[assembly: Guid("4c1b2510-ab65-11f1-93bb-272d31424cdd")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.2.2.0")]
