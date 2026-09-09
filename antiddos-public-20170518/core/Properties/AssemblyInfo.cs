88304fab6a9d2ff42c902054ec44de80dc6a5e67
sdk-team <sdk-team@alibabacloud.com>
2026-09-09 11:35:01 +0000

    Generated csharp 2017-05-18 for antiddos-public.

--- antiddos-public-20170518/core/Properties/AssemblyInfo.cs
+++ antiddos-public-20170518/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("72010450-6977-11f1-9a48-a940cbaf5c7a")]
+[assembly: Guid("e11b9910-ac41-11f1-aec3-5d17f17a0e62")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.10.0")]
-[assembly: AssemblyFileVersion("1.0.11.0")]
+[assembly: AssemblyVersion("1.0.11.0")]
+[assembly: AssemblyFileVersion("1.0.12.0")]
