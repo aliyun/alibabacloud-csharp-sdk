e68b99abeb4436fe462167e407773a3887952f59
sdk-team <sdk-team@alibabacloud.com>
2026-09-19 19:07:46 +0000

    Generated csharp 2026-08-20 for Sandbox.

--- sandbox-20260820/core/Properties/AssemblyInfo.cs
+++ sandbox-20260820/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("34502640-b44f-11f1-ba06-97255f20e836")]
+[assembly: Guid("b8c7f8a0-b45c-11f1-a894-ddcdc93c55c2")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
